abf0ddc097c0fde5382bf3be8e6756d3ad1007a6
Terje Holene <terje.holene@gmail.com>
2021-11-25 14:24:06 +0100

    Drafting database migration folder

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.00/02-setup-grants.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA notifications TO platform_notifications;
+GRANT SELECT,INSERT,UPDATE,REFERENCES,DELETE,TRUNCATE,REFERENCES,TRIGGER ON ALL TABLES IN SCHEMA notifications TO platform_notifications;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA notifications TO platform_notifications;
