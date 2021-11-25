3fbaee073614f85306c07a2e5de1e7a455e4e332
Rune T. Larsen <rune@hjemmekino.no>
2021-11-25 13:24:26 +0100

    Added first controller and first version of external entitites

---
+++ src/Altinn.Notifications.Interfaces/Models/MessageExt.cs
@@ -0,0 +1,29 @@
+﻿namespace Altinn.Notifications.Interfaces.Models
+{
+    /// <summary>
+    /// The message to send to the target
+    /// </summary>
+    public class MessageExt
+    {
+        /// <summary>
+        /// The email title for email notifications
+        /// </summary>
+        public string EmailTitle    { get; set; }
+
+        /// <summary>
+        /// The email body for email notofications. HTML
+        /// </summary>
+        public string EmailBody { get; set; }
+
+        /// <summary>
+        /// SMS Text message
+        /// </summary>
+        public string SmsText { get; set; }
+
+        /// <summary>
+        /// Language 
+        /// </summary>
+        public string Langauge { get; set; }
+
+    }
+}
---
+++ src/Altinn.Notifications.Interfaces/Models/NotificationExt.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Altinn.Notifications.Interfaces.Models
+{
+    public class NotificationExt
+    {
+
+        public string InstanceId { get; set; }
+
+        public List<TargetExt> Targets { get; set; }
+
+        public List<MessageExt> Messages { get; set; }
+
+    }
+}
