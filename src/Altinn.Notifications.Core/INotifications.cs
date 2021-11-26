77f8a807a8839d91af51e570f59f3c4c97787ec4
Rune T. Larsen <rune@hjemmekino.no>
2021-11-26 05:05:50 +0100

    Added interface for core

---
+++ src/Altinn.Notifications.Core/INotifications.cs
@@ -0,0 +1,23 @@
+﻿using Altinn.Notifications.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.Notifications.Core
+{
+    public  interface INotifications
+    {
+
+        public void CreateNotification(Notification notification);
+
+        public List<string> GetSmsTarget();
+
+        public List<string> GetEmailTarget();
+
+        public void Send(string targetId);
+
+        public Notification GetNotification(int notificationId);
+    }
+}
