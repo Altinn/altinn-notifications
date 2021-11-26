f3bc9a490522e8c1164e120f05df80f136df3c20
Rune T. Larsen <rune@hjemmekino.no>
2021-11-26 09:58:07 +0100

    Added notifications service

---
+++ src/Altinn.Notifications.Core/NotificationsService.cs
@@ -0,0 +1,37 @@
+﻿using Altinn.Notifications.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.Notifications.Core
+{
+    internal class NotificationsService : INotifications
+    {
+        public Notification CreateNotification(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetEmailTarget()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notification GetNotification(int notificationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetSmsTarget()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(string targetId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
