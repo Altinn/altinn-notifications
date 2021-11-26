5ce7e3dc210e73f8edbd2e1175b7877cd331b775
Terje Holene <terje.holene@gmail.com>
2021-11-26 15:51:41 +0100

    Make Notification.Sender optional

---
+++ test/Altinn.Notifications.Tests/Persistence/NotificationRepositoryTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Altinn.Notifications.Core.Models;
+using Altinn.Notifications.Persistence;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Moq;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests
+{
+    public  class NotificationRepositoryTests
+    {
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task AddNotification_ActualServer()
+        {
+            Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+
+            PostgreSQLSettings postgreSQLSettings = new() 
+            { 
+                ConnectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password={0};Database=notificationsdb",
+                NotificationsDbPwd = "Password"
+            };
+            
+            Mock<IOptions<PostgreSQLSettings>> options = new();
+            options.Setup(s => s.Value).Returns(postgreSQLSettings);
+
+            NotificationRepository target = new NotificationRepository(options.Object, logger.Object);
+
+            Notification notification = new()
+            {
+                SendTime = DateTime.UtcNow,
+                InstanceId = "partyid/instanceguid",
+                Sender = "sender"
+            };
+
+            Notification actual = await target.AddNotification(notification);
+
+            Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task AddTarget_ActualServer()
+        {
+            Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+
+            PostgreSQLSettings postgreSQLSettings = new()
+            {
+                ConnectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password={0};Database=notificationsdb",
+                NotificationsDbPwd = "Password"
+            };
+
+            Mock<IOptions<PostgreSQLSettings>> options = new();
+            options.Setup(s => s.Value).Returns(postgreSQLSettings);
+
+            NotificationRepository targeted = new NotificationRepository(options.Object, logger.Object);
+
+            Target target = new Target();
+            target.NotificationId = 234;
+            target.ChannelType = "SMS";
+            target.Address = "terje er kul";
+
+            Target actual = await targeted.AddTarget(target);
+
+            Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task AddMessage_ActualServer()
+        {
+            ////const string connectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password=Password;Database=notificationsdb";
+
+            ////Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+            ////NotificationRepository targeted = new NotificationRepository(connectionString, logger.Object);
+
+            ////Message message = new Message();
+            ////message.NotificationId = 234;
+            ////message.EmailSubject = "The coolest";
+            ////message.EmailBody = "terje er kul";
+            ////message.Language = "nb";
+
+            ////Message actual = await targeted.AddMessage(message);
+
+            ////Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task GetNotification_ActualServer()
+        {
+            ////const string connectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password=Password;Database=notificationsdb";
+
+            ////Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+            ////NotificationRepository target = new NotificationRepository(connectionString, logger.Object);
+
+            ////Notification actual = await target.GetNotification(1);
+
+            ////Assert.NotNull(actual);
+
+            ////Assert.Equal("averyvierdinstanceid", actual.InstanceId);
+        }
+
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task GetTarget_ActualServer()
+        {
+            ////const string connectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password=Password;Database=notificationsdb";
+
+            ////Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+            ////NotificationRepository target = new NotificationRepository(connectionString, logger.Object);
+
+            ////Target actual = await target.GetTarget(2);
+
+            ////Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// This test depends on a running database server. It was used to test the data access code and database entities.
+        /// </summary>
+        ///[Fact]
+        public async Task GetUnsentTargets_ActualServer()
+        {
+            ////const string connectionString = "Host=localhost;Port=5432;Username=platform_notifications;Password=Password;Database=notificationsdb";
+
+            ////Mock<ILogger<NotificationRepository>> logger = new Mock<ILogger<NotificationRepository>>();
+            ////NotificationRepository target = new NotificationRepository(connectionString, logger.Object);
+
+            ////List<Target> actual = await target.GetUnsentTargets();
+
+            ////Assert.NotNull(actual);
+        }
+    }
+}
