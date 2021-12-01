439c71c29f01770ebdba8c48b920fa630c018dde
acn-sbuad <stephanie.buadu@avanade.com>
2021-12-01 22:26:39 +0100

    changed email trigger

--- src/Altinn.Notifications.Functions/ScheduleEMail/ScheduleEMail.cs
+++ src/Altinn.Notifications.Functions/ScheduleEMail/ScheduleEMail.cs
@@ -18,7 +18,7 @@ namespace Altinn.Notifications.Functions
         }
 
         [FunctionName("ScheduleEMail")]
-        public async Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 */10 * * * *")] TimerInfo myTimer, ILogger log)
         {
             await _schedule.QueueOutboundEMail();
         }
