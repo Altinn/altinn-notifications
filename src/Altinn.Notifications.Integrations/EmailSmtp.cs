a32894cb6ac60fe489462ba6b6db9b01644e688c
acn-sbuad <stephanie.buadu@avanade.com>
2021-12-06 08:53:51 +0100

    Logging for send email

--- src/Altinn.Notifications.Integrations/EmailSmtp.cs
+++ src/Altinn.Notifications.Integrations/EmailSmtp.cs
@@ -11,19 +11,29 @@ namespace Altinn.Notifications.Integrations
 {
     /// <summary>
     /// Smtp client implementation of IEmail
-    /// </summary>≈
+    /// </summary>
     public class EmailSmtp : IEmail
     {
         private readonly SmtpSettings _smtpSettings;
+        private readonly ILogger<IEmail> _logger;
 
-        public EmailSmtp(IOptions<SmtpSettings> smtpSettings)
+        public EmailSmtp(IOptions<SmtpSettings> smtpSettings, ILogger<IEmail> logger)
         {
             _smtpSettings = smtpSettings.Value;
+            _logger = logger;
+
+            _logger.LogError($"//EmailSmtp // Sender {_smtpSettings.Sender}");
+            _logger.LogError($"//EmailSmtp // Host {_smtpSettings.Host}");
+            _logger.LogError($"//EmailSmtp // Port {_smtpSettings.Port}");
         }
 
         /// <inheritdoc/>
         public async Task<bool> SendEmailAsync(string address, string subject, string body, CancellationToken cancellationToken)
         {
+            _logger.LogError($"//EmailSmtp // Address {address}");
+            _logger.LogError($"//EmailSmtp // Subject {subject}");
+            _logger.LogError($"//EmailSmtp // Body {body}");
+
             MailMessage msg = new MailMessage();
             msg.From = new MailAddress(_smtpSettings.Sender);
             msg.To.Add(new MailAddress(address));
@@ -36,6 +46,5 @@ namespace Altinn.Notifications.Integrations
 
             return true;
         }
-
     }
 }
