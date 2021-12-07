a40ef95006d84a49536bf87bd8ae635522c214e3
Terje Holene <terje.holene@gmail.com>
2021-12-07 12:29:48 +0100

    Added PlatformSettings

---
+++ src/Altinn.Notifications.Integrations/PlatformSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace Altinn.Notifications.Integrations
+{
+    /// <summary>
+    /// Represents settings needed for communication with the Profile components.
+    /// </summary>
+    public class PlatformSettings
+    {
+        /// <summary>
+        /// Gets or sets the Profile component API base url.
+        /// </summary>
+        public string ProfileEndpointAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the name of the subscription key request header.
+        /// </summary>
+        public string ProfileSubscriptionKeyHeaderName { get; set; } = "Ocp-Apim-Subscription-Key";
+
+        /// <summary>
+        /// Gets or sets the API Management subscription key to use in calls to Profile.
+        /// </summary>
+        public string ProfileSubscriptionKey { get; set; } = string.Empty;
+    }
+}
