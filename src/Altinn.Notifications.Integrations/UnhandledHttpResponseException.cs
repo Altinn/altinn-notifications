a40b8b6064dc1877cb5314d9b7b26fb1f795db99
Terje Holene <terje.holene@gmail.com>
2021-12-07 18:04:55 +0100

    Rename UnhandledResponseException and some fixes

--- src/Altinn.Notifications.Integrations/UnhandledHttpResponseException.cs
+++ src/Altinn.Notifications.Integrations/UnhandledHttpResponseException.cs
@@ -1,14 +1,13 @@
-﻿using System.Net;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
-namespace Altinn.App.PlatformServices.Helpers
+namespace Altinn.Notifications.Integrations
 {
     /// <summary>
     /// Represents an exception for when a typed HttpClient recieved an unexpected reponse code and
     /// needs a way to terminate its execution.
     /// </summary>
     [Serializable]
-    public class UnhandledResponseException : Exception
+    public sealed class UnhandledHttpResponseException : Exception
     {
         /// <summary>
         /// The response status code of the unhandled response.
@@ -26,14 +25,14 @@ namespace Altinn.App.PlatformServices.Helpers
         public string Content { get; } = string.Empty;
 
         /// <summary>
-        /// Initialize a new instance of the <see cref="UnhandledResponseException"/> class with default values.
+        /// Initialize a new instance of the <see cref="UnhandledHttpResponseException"/> class with default values.
         /// </summary>
-        public UnhandledResponseException()
+        public UnhandledHttpResponseException()
         {
         }
 
         /// <summary>
-        /// Initialize a new instance of the <see cref="UnhandledResponseException"/> class with the 
+        /// Initialize a new instance of the <see cref="UnhandledHttpResponseException"/> class with the 
         /// given message and custom property values.
         /// </summary>
         /// <remarks>
@@ -42,7 +41,7 @@ namespace Altinn.App.PlatformServices.Helpers
         /// <param name="statusCode">The response status code of the unhandled response.</param>
         /// <param name="reasonPhrase">The reason phrase of the unhandled response.</param>
         /// <param name="content">The content of the unhandled response.</param>
-        private UnhandledResponseException(int statusCode, string reasonPhrase, string content)
+        private UnhandledHttpResponseException(int statusCode, string reasonPhrase, string content)
             : base(FormatMessage(statusCode, reasonPhrase))
         {
             StatusCode = statusCode;
@@ -51,27 +50,27 @@ namespace Altinn.App.PlatformServices.Helpers
         }
 
         /// <summary>
-        /// Initialize a new instance of the <see cref="UnhandledResponseException"/> class using the
+        /// Initialize a new instance of the <see cref="UnhandledHttpResponseException"/> class using the
         /// given <see cref="SerializationInfo"/>.
         /// </summary>
-        private UnhandledResponseException(SerializationInfo info, StreamingContext context) 
+        private UnhandledHttpResponseException(SerializationInfo info, StreamingContext context) 
             : base(info, context)
         {
             StatusCode = (int)info.GetValue(nameof(StatusCode), typeof(int))!;
             ReasonPhrase = (string)info.GetValue(nameof(ReasonPhrase), typeof(string))!;
-            Content = (string)info.GetValue(nameof(Content), typeof(string))!;
+            // Property Content excluded on purpose. It might contain data we don't want stored in logs.
         }
 
         /// <summary>
-        /// Create a new <see cref="UnhandledResponseException"/> by reading the <see cref="HttpResponseMessage"/>
+        /// Create a new <see cref="UnhandledHttpResponseException"/> by reading the <see cref="HttpResponseMessage"/>
         /// content asynchronously.
         /// </summary>
         /// <param name="response">The <see cref="HttpResponseMessage"/> to read.</param>
-        /// <returns>A new <see cref="UnhandledResponseException"/>.</returns>
-        public static async Task<UnhandledResponseException> CreateAsync(HttpResponseMessage response)
+        /// <returns>A new <see cref="UnhandledHttpResponseException"/>.</returns>
+        public static async Task<UnhandledHttpResponseException> CreateAsync(HttpResponseMessage response)
         {
             string content = await response.Content.ReadAsStringAsync();
-            return new UnhandledResponseException((int)response.StatusCode, response.ReasonPhrase!, content);
+            return new UnhandledHttpResponseException((int)response.StatusCode, response.ReasonPhrase!, content);
         }
 
         /// <summary>
@@ -83,7 +82,7 @@ namespace Altinn.App.PlatformServices.Helpers
         {
             info.AddValue(nameof(StatusCode), StatusCode);
             info.AddValue(nameof(ReasonPhrase), ReasonPhrase);
-            info.AddValue(nameof(Content), Content);
+            // Property Content excluded on purpose. It might contain data we don't want stored in logs.
             base.GetObjectData(info, context);
         }
 
