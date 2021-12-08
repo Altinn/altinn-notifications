4259e1821c45e0e00e66bfa029287f7175d939a3
Terje Holene <terje.holene@gmail.com>
2021-12-08 16:35:45 +0100

    Added GeneralSettings and an implementation of IUserTokenProvider.

---
+++ src/Altinn.Notifications/Configuration/GeneralSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Altinn.Notifications.Configuration
+{
+    /// <summary>
+    /// Represents settings needed by the application for various purposes...
+    /// </summary>
+    public class GeneralSettings
+    {
+        /// <summary>
+        /// Gets or sets the name of the JSON Web Token cookie.
+        /// </summary>
+        public string JwtCookieName { get; set; } = string.Empty;
+    }
+}
---
+++ src/Altinn.Notifications/Services/UserTokenProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Altinn.Notifications.Configuration;
+using Altinn.Notifications.Integrations;
+
+using AltinnCore.Authentication.Utils;
+
+using Microsoft.Extensions.Options;
+
+namespace Altinn.Notifications.Services
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IUserTokenProvider"/> using the HttpContext to obtain
+    /// the JSON Web Token needed for the application to make calls to other services.
+    /// </summary>
+    /// <remarks>
+    /// This class is excluded from code doverage because we have no good way of mocking the HttpContext. 
+    /// There are also very little code to test as most of the logic are in an imported package.
+    /// </remarks>
+    [ExcludeFromCodeCoverage]
+    public class UserTokenProvider : IUserTokenProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _jwtCookieName = string.Empty;
+
+        public UserTokenProvider(IHttpContextAccessor httpContextAccessor, IOptions<GeneralSettings> generalSettings)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _jwtCookieName = generalSettings.Value.JwtCookieName;
+
+        }
+
+        /// <summary>
+        /// Get the current JSON Web Token found on the HttpContext.
+        /// </summary>
+        /// <returns>The JSON Web Token of the current user.</returns>
+        public string GetUserToken()
+        {
+            return JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _jwtCookieName);
+        }
+    }
+}
