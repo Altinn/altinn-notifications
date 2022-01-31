103b81a9d819156eb2ac05d7ded961d802908ade
Terje Holene <terje.holene@gmail.com>
2022-01-31 17:38:42 +0100

    Initial connectionstring and password values

--- src/Altinn.Notifications.Persistence/PostgreSQLSettings.cs
+++ src/Altinn.Notifications.Persistence/PostgreSQLSettings.cs
@@ -1,15 +1,18 @@
 ﻿namespace Altinn.Notifications.Persistence
 {
+    /// <summary>
+    /// Represents settings needed to communicate with the PostgreSQL database server.
+    /// </summary>
     public class PostgreSQLSettings
     {
         /// <summary>
-        /// Connection string for the postgres db
+        /// Connection string to the PostgresSQL database server.
         /// </summary>
-        public string ConnectionString { get; set; }
+        public string ConnectionString { get; set; } = string.Empty;
 
         /// <summary>
-        /// Password for app user for the postgres db
+        /// The application database user password. Username already a part of the connection string.
         /// </summary>
-        public string NotificationsDbPwd { get; set; }
+        public string NotificationsDbPwd { get; set; } = string.Empty;
     }
 }
