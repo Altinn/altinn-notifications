433bbf546812d89702e927a5a6a4c2f75d0c4572
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-05-16 15:49:31 +0200

    added support for application insights and health check (#4)

--- components/email-service/src/Altinn.Notifications.Email/Program.cs
+++ components/email-service/src/Altinn.Notifications.Email/Program.cs
@@ -1,25 +1,157 @@
+using Altinn.Notifications.Email.Configuration;
+using Altinn.Notifications.Email.Health;
+
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+ILogger logger;
+const string AppInsightsKeyName = "ApplicationInsights--InstrumentationKey";
+string applicationInsightsConnectionString = string.Empty;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
+ConfigureWebHostCreationLogging();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+await SetConfigurationProviders(builder.Configuration);
+
+ConfigureApplicationLogging(builder.Logging);
+
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+Configure();
 
 app.Run();
+
+void ConfigureWebHostCreationLogging()
+{
+    var logFactory = LoggerFactory.Create(builder =>
+    {
+        builder
+            .AddFilter("Altinn.Notifications.Email.Program", LogLevel.Debug)
+            .AddConsole();
+    });
+
+    logger = logFactory.CreateLogger<Program>();
+}
+
+async Task SetConfigurationProviders(ConfigurationManager config)
+{
+    string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+    config.SetBasePath(basePath);
+    string configJsonFile1 = $"{basePath}/altinn-appsettings/altinn-dbsettings-secret.json";
+    string configJsonFile2 = Directory.GetCurrentDirectory() + "/appsettings.json";
+
+    if (basePath == "/")
+    {
+        configJsonFile2 = "/app/appsettings.json";
+    }
+
+    config.AddJsonFile(configJsonFile1, optional: true, reloadOnChange: true);
+    config.AddJsonFile(configJsonFile2, optional: false, reloadOnChange: true);
+
+    await ConnectToKeyVaultAndSetApplicationInsights(config);
+
+    config.AddEnvironmentVariables();
+    config.AddCommandLine(args);
+}
+
+async Task ConnectToKeyVaultAndSetApplicationInsights(ConfigurationManager config)
+{
+    KeyVaultSettings keyVaultSettings = new();
+    config.GetSection("kvSetting").Bind(keyVaultSettings);
+    if (!string.IsNullOrEmpty(keyVaultSettings.ClientId) &&
+        !string.IsNullOrEmpty(keyVaultSettings.TenantId) &&
+        !string.IsNullOrEmpty(keyVaultSettings.ClientSecret) &&
+        !string.IsNullOrEmpty(keyVaultSettings.SecretUri))
+    {
+        logger.LogInformation("Program // Configure key vault client // App");
+        Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", keyVaultSettings.ClientId);
+        Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", keyVaultSettings.ClientSecret);
+        Environment.SetEnvironmentVariable("AZURE_TENANT_ID", keyVaultSettings.TenantId);
+        var azureCredentials = new DefaultAzureCredential();
+
+        config.AddAzureKeyVault(new Uri(keyVaultSettings.SecretUri), azureCredentials);
+
+        SecretClient client = new(new Uri(keyVaultSettings.SecretUri), azureCredentials);
+
+        try
+        {
+            KeyVaultSecret keyVaultSecret = await client.GetSecretAsync(AppInsightsKeyName);
+            applicationInsightsConnectionString = string.Format("InstrumentationKey={0}", keyVaultSecret.Value);
+        }
+        catch (Exception vaultException)
+        {
+            logger.LogError(vaultException, $"Unable to read application insights key.");
+        }
+    }
+}
+
+void ConfigureApplicationLogging(ILoggingBuilder logging)
+{
+    // The default ASP.NET Core project templates call CreateDefaultBuilder, which adds the following logging providers:
+    // Console, Debug, EventSource
+    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1
+
+    // Clear log providers
+    logging.ClearProviders();
+
+    // Setup up application insight if applicationInsightsConnectionString is available
+    if (!string.IsNullOrEmpty(applicationInsightsConnectionString))
+    {
+        // Add application insights https://docs.microsoft.com/en-us/azure/azure-monitor/app/ilogger
+        logging.AddApplicationInsights(
+            configureTelemetryConfiguration: (config) => config.ConnectionString = applicationInsightsConnectionString,
+            configureApplicationInsightsLoggerOptions: (options) => { });
+
+        // Optional: Apply filters to control what logs are sent to Application Insights.
+        // The following configures LogLevel Information or above to be sent to
+        // Application Insights for all categories.
+        logging.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Warning);
+
+        // Adding the filter below to ensure logs of all severity from Program.cs
+        // is sent to ApplicationInsights.
+        logging.AddFilter<ApplicationInsightsLoggerProvider>(typeof(Program).FullName, LogLevel.Trace);
+    }
+    else
+    {
+        // If not application insight is available log to console
+        logging.AddFilter("Microsoft", LogLevel.Warning);
+        logging.AddFilter("System", LogLevel.Warning);
+    }
+
+    logging.AddConsole();
+}
+
+void ConfigureServices(IServiceCollection services, ConfigurationManager configuration)
+{
+    services.AddControllers();
+    services.AddHealthChecks().AddCheck<HealthCheck>("notifications_emails_health_check");
+
+    if (!string.IsNullOrEmpty(applicationInsightsConnectionString))
+    {
+        services.AddSingleton(typeof(ITelemetryChannel), new ServerTelemetryChannel { StorageFolder = "/tmp/logtelemetry" });
+        services.AddApplicationInsightsTelemetryWorkerService(new Microsoft.ApplicationInsights.WorkerService.ApplicationInsightsServiceOptions
+        {
+            ConnectionString = applicationInsightsConnectionString
+        });
+
+        // services.AddApplicationInsightsTelemetryProcessor<HealthTelemetryFilter>();
+        services.AddSingleton<ITelemetryInitializer, CustomTelemetryInitializer>();
+
+        logger.LogInformation("Program // ApplicationInsightsTelemetryKey = {applicationInsightsConnectionString}", applicationInsightsConnectionString);
+    }
+}
+
+void Configure()
+{
+    app.UseAuthorization();
+    app.MapControllers();
+    app.MapHealthChecks("/health");
+}
