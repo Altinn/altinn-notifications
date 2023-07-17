73c80c91d4f38b9f20f4b0e5e2d7a9b23b4266e9
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-17 15:37:07 +0200

    not setting up email notifications table for now

--- src/Altinn.Notifications.Persistence/Migration/v0.01/03-setup-tables.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.01/03-setup-tables.sql
@@ -11,20 +11,6 @@ CREATE TABLE IF NOT EXISTS notifications.orders
 	notificationorder JSONB NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS notifications.emailnotifications
-(
-	_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	alternateid UUID UNIQUE NOT NULL,
-	_orderid BIGINT REFERENCES notifications.orders(_id) ON DELETE CASCADE,
-	recipientid TEXT,
-	toaddress TEXT NOT NULL,
-	result TEXT NOT NULL,
-	resulttime TIMESTAMPTZ NOT NULL,
-	expirytime TIMESTAMPTZ NOT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS notifications.emailtexts
 (
 	_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
