0791b84e20e4024481370243110d1b5098ad716d
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-18 17:05:11 +0200

    changed to webapi

--- src/Altinn.Notifications.Triggers/Program.cs
+++ src/Altinn.Notifications.Triggers/Program.cs
@@ -1,7 +1,28 @@
+using Altinn.Notifications.Triggers.Health;
+
+var builder = WebApplication.CreateBuilder(args);
+
 IConfigurationRoot configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
 
 string notificationsEndpoint = configuration["PlatformSettings:ApiNotificationsEndpoint"]!;
-
 Console.WriteLine($"Hello, World! \r\n {notificationsEndpoint}");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks().AddCheck<HealthCheck>("notifications_triggers_health_check");
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHealthChecks("/health");
+
+
+app.Run();
