47ccad5eb53890141d9ce7952bd7a55b1be095a8
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-18 15:46:14 +0200

    fixed code smell

--- src/Altinn.Notifications.Triggers/Program.cs
+++ src/Altinn.Notifications.Triggers/Program.cs
@@ -1,9 +1,7 @@
-// See https://aka.ms/new-console-template for more information
-
 IConfigurationRoot configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
 
-string notificationsEndpoint = configuration["PlatformSettings:ApiNotificationsEndpoint"];
+string notificationsEndpoint = configuration["PlatformSettings:ApiNotificationsEndpoint"]!;
 
 Console.WriteLine($"Hello, World! \r\n {notificationsEndpoint}");
