c659dce2af61e67137882ad958c8c64d0625b17b
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-18 11:49:58 +0200

    fixed supression syntax

--- src/Altinn.Notifications.Core/Enums/AddressType.cs
+++ src/Altinn.Notifications.Core/Enums/AddressType.cs
@@ -1,11 +1,10 @@
 ﻿namespace Altinn.Notifications.Core.Enums;
-
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 /// <summary>
 /// Enum describing available address types
 /// </summary>
 public enum AddressType
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     Email
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
