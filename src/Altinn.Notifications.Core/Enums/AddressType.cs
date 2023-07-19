268f4734e05e99c5bde1cef8d76e1ea2feae15a3
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-19 14:53:53 +0200

    merged file from main

--- src/Altinn.Notifications.Core/Enums/AddressType.cs
+++ src/Altinn.Notifications.Core/Enums/AddressType.cs
@@ -5,6 +5,6 @@
 /// </summary>
 public enum AddressType
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     Email
 }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
