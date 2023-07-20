80bce3ccbbb91b389c6632adac672d0ee9b22e72
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-20 14:52:23 +0200

    new endpoint for trigger sending of email notifications

--- src/Altinn.Notifications/Controllers/TriggerController.cs
+++ src/Altinn.Notifications/Controllers/TriggerController.cs
@@ -1,4 +1,6 @@
-﻿using Altinn.Notifications.Core.Services.Interfaces;
+﻿using System.Runtime.CompilerServices;
+
+using Altinn.Notifications.Core.Services.Interfaces;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,13 +15,15 @@ namespace Altinn.Notifications.Controllers;
 public class TriggerController : ControllerBase
 {
     private readonly IOrderProcessingService _orderProcessingService;
+    private readonly IEmailNotificationService _emailNotificationService;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="TriggerController"/> class.
     /// </summary>
-    public TriggerController(IOrderProcessingService orderProcessingService)
+    public TriggerController(IOrderProcessingService orderProcessingService, IEmailNotificationService emailNotificationService)
     {
         _orderProcessingService = orderProcessingService;
+        _emailNotificationService = emailNotificationService;
     }
 
     /// <summary>
@@ -27,10 +31,22 @@ public class TriggerController : ControllerBase
     /// </summary>
     [HttpPost]
     [Consumes("application/json")]
-    [Route("pastdueorders")]    
+    [Route("pastdueorders")]
     public async Task<ActionResult> Trigger_PastDueOrders()
     {
         await _orderProcessingService.StartProcessingPastDueOrders();
+        return Ok();
+    }
+
+    /// <summary>
+    /// Endpoint for retrieving email notifications that are ready to send
+    /// </summary>
+    [HttpPost]
+    [Consumes("application/json")]
+    [Route("sendemail")]
+    public async Task<ActionResult> Trigger_SendEmailNotifications()
+    {
+        await _emailNotificationService.SendNotifications();
         return Ok();
     }
 }
