6bbf5daf59b2366904add3887d70f962cf16ceb4
acn-sbuad <stephanie.buadu@avanade.com>
2023-07-24 09:23:23 +0200

    inserted bug in unit test to check workflow

--- test/Altinn.Notifications.Tests/Notifications/TestingModels/NotificationOrderExtTests.cs
+++ test/Altinn.Notifications.Tests/Notifications/TestingModels/NotificationOrderExtTests.cs
@@ -60,7 +60,7 @@ public class NotificationOrderExtTests
             {
                 FromAddress = "sender@domain.com",
                 Subject = "email-subject",
-                Body = "email-body",
+                Body = "email-boy",
                 ContentType = EmailContentType.Html,
             }
         };
