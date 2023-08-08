05a5d00e5f074925b687ad2ead866edb2d5eddd3
acn-sbuad <stephanie.buadu@avanade.com>
2023-08-08 15:34:08 +0200

    using custom serializer for email model

---
+++ test/Altinn.Notifications.Tests/Notifications.Core/TestingModels/EmailTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json.Nodes;
+
+using Altinn.Notifications.Core.Enums;
+using Altinn.Notifications.Core.Models;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.Core.TestingModels;
+public class EmailTests
+{
+    private readonly Email _email;
+    private readonly string _serializedEmail;
+
+    public EmailTests()
+    {
+        Guid id = Guid.NewGuid();
+        _email = new Email(id, "subject", "body", "from@domain.com", "to@domain.com", EmailContentType.Html);
+        _serializedEmail = new JsonObject()
+        {
+            { "id", id },
+             { "subject", "subject" },
+            {"body", "body" },
+            {"fromAddress", "from@domain.com" },
+            {"toAddress", "to@domain.com" },
+            {"contentType", "Html" },
+
+        }.ToJsonString();
+    }
+
+
+    [Fact]
+    public void SerializeToJson()
+    {
+        // Arrange
+        string expected = _serializedEmail;
+
+        // Act
+        var actual = _email.Serialize();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
