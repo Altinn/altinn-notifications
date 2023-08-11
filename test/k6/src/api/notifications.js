4131aa09f09c96bbbaa47d6bdf7f17eb7c3c7a78
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-08-11 15:41:27 +0200

    Automated k6 tests and workflow (#199)

---
+++ test/k6/src/api/notifications.js
@@ -0,0 +1,35 @@
+import http from "k6/http";
+
+import * as config from "../config.js";
+
+import * as apiHelpers from "../apiHelpers.js";
+
+export function postEmailNotificationOrder(serializedOrder, token) {
+  var endpoint = config.notifications.orders_email;
+
+  var params = apiHelpers.buildHeaderWithBearerAndContentType(token);
+
+  var response = http.post(endpoint, serializedOrder, params);
+
+  return response;
+}
+
+export function getOrderById(id, token) {
+  var endpoint = config.notifications.orders + id;
+  return getOrderByUrl(endpoint, queryParams, token);
+}
+
+export function getOrderByUrl(url, token) {
+  var params = apiHelpers.buildHeaderWithBearer(token);
+  var response = http.get(url, params);
+
+  return response;
+}
+
+export function getOrderBySendersReference(sendersReference, token) {
+  var endpoint =
+    config.notifications.orders + "?sendersReference=" + sendersReference;
+  var params = apiHelpers.buildHeaderWithBearer(token);
+  var response = http.get(endpoint, params);
+  return response;
+}
---
+++ test/k6/src/config.js
@@ -0,0 +1,24 @@
+// Baseurls for platform
+export var baseUrls = {
+  at21: "at21.altinn.cloud",
+  at22: "at22.altinn.cloud",
+  at23: "at23.altinn.cloud",
+  at24: "at24.altinn.cloud",
+  tt02: "tt02.altinn.no"
+};
+
+//Get values from environment
+const environment = __ENV.env.toLowerCase();
+export let baseUrl = baseUrls[environment];
+
+//AltinnTestTools
+export var tokenGenerator = {
+  getEnterpriseToken:
+    "https://altinn-testtools-token-generator.azurewebsites.net/api/GetEnterpriseToken",
+};
+
+// Platform Notifications
+export var notifications = {
+  orders: "https://platform." + baseUrl + "/notifications/api/v1/orders/",
+  orders_email: "https://platform." + baseUrl + "/notifications/api/v1/orders/email/"
+};
