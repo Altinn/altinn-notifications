f4863cc28d670655df15c1d7f31f0b97d23fc3c0
tba76 <thomabak@gmail.com>
2023-09-06 13:08:48 +0200

    Push to topic after fail

--- src/Altinn.Notifications.Integrations/Kafka/Consumers/EmailStatusConsumer.cs
+++ src/Altinn.Notifications.Integrations/Kafka/Consumers/EmailStatusConsumer.cs
@@ -1,4 +1,5 @@
-﻿using Altinn.Notifications.Core.Models.Notification;
+﻿using Altinn.Notifications.Core.Integrations.Interfaces;
+using Altinn.Notifications.Core.Models.Notification;
 using Altinn.Notifications.Core.Services.Interfaces;
 using Altinn.Notifications.Integrations.Configuration;
 
@@ -13,14 +14,22 @@ namespace Altinn.Notifications.Integrations.Kafka.Consumers;
 public class EmailStatusConsumer : KafkaConsumerBase<EmailStatusConsumer>
 {
     private readonly IEmailNotificationService _emailNotificationsService;
+    private readonly IKafkaProducer _producer;
+    private readonly string _topicName;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EmailStatusConsumer"/> class.
     /// </summary>
-    public EmailStatusConsumer(IEmailNotificationService emailNotificationsService, IOptions<KafkaSettings> settings, ILogger<EmailStatusConsumer> logger)
+    public EmailStatusConsumer(
+        IEmailNotificationService emailNotificationsService,
+        IKafkaProducer producer, 
+        IOptions<KafkaSettings> settings, 
+        ILogger<EmailStatusConsumer> logger)
         : base(settings, logger, settings.Value.EmailStatusUpdatedTopicName)
     {
         _emailNotificationsService = emailNotificationsService;
+        _producer = producer;
+        _topicName = settings.Value.EmailStatusUpdatedTopicName;
     }
 
     /// <inheritdoc/>
@@ -43,7 +52,6 @@ public class EmailStatusConsumer : KafkaConsumerBase<EmailStatusConsumer>
 
     private async Task RetryOrder(string message)
     {
-        // Retry logic - back to kafka?
-        await Task.CompletedTask;
+        await _producer.ProduceAsync(_topicName, message!);
     }
 }
