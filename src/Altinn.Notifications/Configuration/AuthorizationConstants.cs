3e80ada4d1e3058532bc92955f9a5806a97f5825
Terje Holene <terje.holene@gmail.com>
2023-10-06 16:21:55 +0200

    Add AuthorizationConstants

---
+++ src/Altinn.Notifications/Configuration/AuthorizationConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Notifications.Configuration;
+
+/// <summary>
+/// Constants used in authorization of access to Notifications.
+/// </summary>
+public static class AuthorizationConstants
+{
+    /// <summary>
+    /// Id for policy for allowing configuration administration
+    /// </summary>
+    public const string POLICY_SCOPE_CONFIG_ADMIN = "PolicyScopeConfigAdmin";
+
+    /// <summary>
+    /// Scope for allowing administration of configuration
+    /// </summary>
+    public const string SCOPE_CONFIG_ADMIN = "altinn:notifications.config.admin";
+}
