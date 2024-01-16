31b435ef3146f88e6387be3014346fdb36d6d4b0
Khan Mohammad R. <codemascot@hotmail.com>
2024-01-16 18:18:35 +0100

    Fixed the code documentation or comment

--- src/Altinn.Notifications.Core/Services/Interfaces/IOrderRequestService.cs
+++ src/Altinn.Notifications.Core/Services/Interfaces/IOrderRequestService.cs
@@ -4,14 +4,14 @@ using Altinn.Notifications.Core.Models.Orders;
 namespace Altinn.Notifications.Core.Services.Interfaces;
 
 /// <summary>
-/// Interface for the email notification order service
+/// Interface for the notification order service
 /// </summary>
 public interface IOrderRequestService
 {
     /// <summary>
     /// Registers a new order
     /// </summary>
-    /// <param name="orderRequest">The email notification order request</param>
+    /// <param name="orderRequest">The notification order request</param>
     /// <returns>The registered notification order</returns>
     public Task<(NotificationOrder? Order, ServiceError? Error)> RegisterNotificationOrder(NotificationOrderRequest orderRequest);
 }
