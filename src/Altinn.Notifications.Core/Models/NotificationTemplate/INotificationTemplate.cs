81868e511dd9b59161ed14cb093926fe82c362f2
Khan Mohammad R. <codemascot@hotmail.com>
2024-01-16 12:07:45 +0100

    Added JSON subtype for SMS in the notification interface

--- src/Altinn.Notifications.Core/Models/NotificationTemplate/INotificationTemplate.cs
+++ src/Altinn.Notifications.Core/Models/NotificationTemplate/INotificationTemplate.cs
@@ -8,6 +8,7 @@ namespace Altinn.Notifications.Core.Models.NotificationTemplate;
 /// Base class for a notification template
 /// </summary>
 [JsonDerivedType(typeof(EmailTemplate), "email")]
+[JsonDerivedType(typeof(SmsTemplate), "sms")]
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "$")]
 public interface INotificationTemplate
 {
