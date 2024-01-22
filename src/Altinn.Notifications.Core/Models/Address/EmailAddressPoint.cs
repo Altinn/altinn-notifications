bb994429c37e82ef683eb3c253b49287fe24bf2d
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2024-01-22 13:12:56 +0100

    #336 added mobilenumber address point class #GCPActive (#383)
    * #336 added mobilenumber address point class #GCPActive
    
    * fixed addresstype enum #GCPActive
    
    * rolled back action version

--- src/Altinn.Notifications.Core/Models/Address/EmailAddressPoint.cs
+++ src/Altinn.Notifications.Core/Models/Address/EmailAddressPoint.cs
@@ -3,7 +3,7 @@
 namespace Altinn.Notifications.Core.Models.Address;
 
 /// <summary>
-/// A class represeting an address point
+/// A class represeting an email address point
 /// </summary>
 public class EmailAddressPoint : IAddressPoint
 {
