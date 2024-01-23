c7114b1af912ba5a1af5571360a12d33bcb7c859
Khan Mohammad R. <codemascot@hotmail.com>
2024-01-23 12:44:53 +0100

    Validator test initiated for SMS notifiaction order request

---
+++ test/Altinn.Notifications.Tests/Notifications/TestingValidators/SmsNotificationOrderRequestValidatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Altinn.Notifications.Models;
+using Altinn.Notifications.Validators;
+
+using FluentValidation;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.TestingValidators;
+
+public class SmsNotificationOrderRequestValidatorTests
+{
+    private readonly SmsNotificationOrderRequestValidator _validator;
+
+    public SmsNotificationOrderRequestValidatorTests()
+    {
+        ValidatorOptions.Global.LanguageManager.Enabled = false;
+        _validator = new SmsNotificationOrderRequestValidator();
+    }
+}
