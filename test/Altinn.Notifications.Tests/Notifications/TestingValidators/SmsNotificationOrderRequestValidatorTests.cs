4eba881cbf6c9e7677a7c9ac858365b99936e49c
Khan Mohammad R. <codemascot@hotmail.com>
2024-01-23 12:46:35 +0100

    Validatee recipient provided for SMS return true added

--- test/Altinn.Notifications.Tests/Notifications/TestingValidators/SmsNotificationOrderRequestValidatorTests.cs
+++ test/Altinn.Notifications.Tests/Notifications/TestingValidators/SmsNotificationOrderRequestValidatorTests.cs
@@ -19,4 +19,18 @@ public class SmsNotificationOrderRequestValidatorTests
         ValidatorOptions.Global.LanguageManager.Enabled = false;
         _validator = new SmsNotificationOrderRequestValidator();
     }
+
+    [Fact]
+    public void Validate_RecipientProvidedForSms_ReturnsTrue()
+    {
+        var order = new SmsNotificationOrderRequestExt()
+        {
+            SenderNumber = "+4740000000",
+            Recipients = new List<RecipientExt>() { new RecipientExt() { MobileNumber = "+4740000001" } },
+            Body = "This is an SMS body"
+        };
+
+        var actual = _validator.Validate(order);
+        Assert.True(actual.IsValid);
+    }
 }
