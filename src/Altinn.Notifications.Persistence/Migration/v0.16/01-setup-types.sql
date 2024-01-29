e50f761df4d252cdc5562a0c7537e318d1708479
tba76 <thomabak@gmail.com>
2024-01-29 14:46:44 +0100

    Removed failed enum

--- src/Altinn.Notifications.Persistence/Migration/v0.16/01-setup-types.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.16/01-setup-types.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'smsnotificationresulttype') THEN
-        CREATE TYPE smsnotificationresulttype AS ENUM ('New', 'Sending', 'Succeeded', 'Failed_InvalidReceiver', 'Failed');
+        CREATE TYPE smsnotificationresulttype AS ENUM ('New', 'Sending', 'Succeeded', 'Failed_InvalidReceiver');
     END IF;
 END$$;
