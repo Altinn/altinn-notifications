542dda41a37e17f9015f9d79a71f41175ba21a89
tba76 <thomabak@gmail.com>
2024-01-29 14:48:17 +0100

    Adjust enum

--- src/Altinn.Notifications.Persistence/Migration/v0.16/01-setup-types.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.16/01-setup-types.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'smsnotificationresulttype') THEN
-        CREATE TYPE smsnotificationresulttype AS ENUM ('New', 'Sending', 'Succeeded', 'Failed_InvalidReceiver');
+        CREATE TYPE smsnotificationresulttype AS ENUM ('New', 'Sending', 'Accepted', 'Failed_InvalidReceiver', 'Failed');
     END IF;
 END$$;
