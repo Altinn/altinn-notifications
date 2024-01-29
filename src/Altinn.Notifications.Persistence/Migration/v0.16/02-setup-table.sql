341331b45adc8df4c4eb92babdad5d101f368d1d
tba76 <thomabak@gmail.com>
2024-01-29 14:44:39 +0100

    Add new table, smsnotifications

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.16/02-setup-table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE IF NOT EXISTS notifications.smsnotifications
+(
+	_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	_orderid BIGINT REFERENCES notifications.orders(_id) ON DELETE CASCADE NOT NULL,
+	alternateid UUID UNIQUE NOT NULL,
+	recipientid TEXT,
+	mobilenumber TEXT NOT NULL,
+	result smsnotificationresulttype NOT NULL,
+	gatewayreference TEXT,
+	segments INT,
+	resulttime TIMESTAMPTZ NOT NULL,
+	expirytime TIMESTAMPTZ NOT NULL	
+);
+
+GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE notifications.smsnotifications TO platform_notifications;
