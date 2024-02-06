b4f9af817e433ff966913d8752b4fa87fe627513
acn-sbuad <stephanie.buadu@avanade.com>
2024-02-06 12:30:27 +0100

    undid persistence interface

--- src/Altinn.Notifications.Core/Persistence/INotificationSummaryRepository.cs
+++ src/Altinn.Notifications.Core/Persistence/INotificationSummaryRepository.cs
@@ -12,10 +12,4 @@ public interface INotificationSummaryRepository
     /// </summary>
     /// <returns>A partial email notification summary object</returns>
     public Task<EmailNotificationSummary?> GetEmailSummary(Guid orderId, string creator);
-
-    /// <summary>
-    /// Retrieves all sms notifications for the provided order id in an sms notification summary
-    /// </summary>
-    /// <returns>A partial sms notification summary object</returns>
-    public Task<SmsNotificationSummary?> GetSmsSummary(Guid orderId, string creator);
 }
