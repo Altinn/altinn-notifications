9a6cda3b74bfe909248fc0b3b1b5066653008e57
acn-sbuad <stephanie.buadu@avanade.com>
2024-02-06 12:29:48 +0100

    initial implementation

--- src/Altinn.Notifications.Persistence/Repository/NotificationSummaryRepository.cs
+++ src/Altinn.Notifications.Persistence/Repository/NotificationSummaryRepository.cs
@@ -20,7 +20,8 @@ public class NotificationSummaryRepository : INotificationSummaryRepository
     private readonly NpgsqlDataSource _dataSource;
     private readonly TelemetryClient? _telemetryClient;
 
-    private const string _getNotificationsByOrderIdSql = "select * from notifications.getemailsummary($1, $2)"; // (_alternateorderid, creatorname)
+    private const string _getEmailNotificationsByOrderIdSql = "select * from notifications.getemailsummary($1, $2)"; // (_alternateorderid, creatorname)
+    private const string _getSmsNotificationsByOrderIdSql = "select * from notifications.getsmssummary($1, $2)"; // (_alternateorderid, creatorname)
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EmailNotificationRepository"/> class.
@@ -41,7 +42,7 @@ public class NotificationSummaryRepository : INotificationSummaryRepository
         List<EmailNotificationWithResult> notificationList = new();
         string sendersReference = string.Empty;
 
-        await using NpgsqlCommand pgcom = _dataSource.CreateCommand(_getNotificationsByOrderIdSql);
+        await using NpgsqlCommand pgcom = _dataSource.CreateCommand(_getEmailNotificationsByOrderIdSql);
         using TelemetryTracker tracker = new(_telemetryClient, pgcom);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, orderId);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, creator);
@@ -89,5 +90,63 @@ public class NotificationSummaryRepository : INotificationSummaryRepository
         };
 
         return emailSummary;
+    }
+
+    /// <inheritdoc/>
+    public async Task<SmsNotificationSummary?> GetSmsSummary(Guid orderId, string creator)
+    {
+        bool matchFound = false;
+
+        List<SmsNotificationWithResult> notificationList = new();
+        string sendersReference = string.Empty;
+
+        await using NpgsqlCommand pgcom = _dataSource.CreateCommand(_getSmsNotificationsByOrderIdSql);
+        using TelemetryTracker tracker = new(_telemetryClient, pgcom);
+        pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, orderId);
+        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, creator);
+
+        await using (NpgsqlDataReader reader = await pgcom.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                if (!matchFound)
+                {
+                    matchFound = true;
+                    sendersReference = reader.GetValue<string>("sendersreference");
+                }
+
+                Guid notificationId = reader.GetValue<Guid>("alternateid");
+
+                if (notificationId != Guid.Empty)
+                {
+                    notificationList.Add(
+                    new SmsNotificationWithResult(
+                         reader.GetValue<Guid>("alternateid"),
+                         new SmsRecipient()
+                         {
+                             RecipientId = reader.GetValue<string>("recipientid"),
+                             MobileNumber = reader.GetValue<string>("mobilenumber")
+                         },
+                         new NotificationResult<SmsNotificationResultType>(
+                            reader.GetValue<SmsNotificationResultType>("result"),
+                            reader.GetValue<DateTime>("resulttime"))));
+                }
+            }
+        }
+
+        tracker.Track();
+
+        if (!matchFound)
+        {
+            return null;
+        }
+
+        SmsNotificationSummary smsSummary = new(orderId)
+        {
+            SendersReference = sendersReference,
+            Notifications = notificationList
+        };
+
+        return smsSummary;
     }
 }
