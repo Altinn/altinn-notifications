24c0c35822daab435b47acf0abb5204602a44a7f
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2024-02-09 15:08:21 +0100

    added indexes for orders and sms tables (#424)
    * added indexes for orders and sms tables
    
    * removed notifications prefix for index
    
    * notifications_ prefix for all indexes

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.22/01-setup-indexes.sql
@@ -0,0 +1,7 @@
+CREATE INDEX IF NOT EXISTS notifications_orders_alternateid_creatorname ON notifications.orders (alternateid, creatorname);
+
+
+CREATE INDEX IF NOT EXISTS notifications_smsnotifications_orderid ON notifications.smsnotifications (_orderid) include (_id, result);
+CREATE INDEX IF NOT EXISTS notifications_smsnotifications_recipient ON notifications.smsnotifications (recipientid, mobilenumber) include (_id);
+CREATE INDEX IF NOT EXISTS notifications_smsnotifications_result ON notifications.smsnotifications (result) include (_id, _orderid);
+CREATE INDEX IF NOT EXISTS notifications_smstexts_orderid ON notifications.smstexts (_orderid);
