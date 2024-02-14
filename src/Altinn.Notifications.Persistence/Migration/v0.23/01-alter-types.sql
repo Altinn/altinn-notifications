09875054ed56f11fbe8a5928205e38bb3088f291
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2024-02-14 13:39:14 +0100

    New status codes for SMS (#433)
    * cleaned up obsolete function
    
    * added new sms status codes

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.23/01-alter-types.sql
@@ -0,0 +1,6 @@
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Delivered';
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Failed_BarredReceiver';
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Failed_Deleted';
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Failed_Expired';
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Failed_Undelivered';
+ALTER TYPE smsnotificationresulttype ADD VALUE 'Failed_Rejected';
