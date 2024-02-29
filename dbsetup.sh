8d0882664fc0053a7d7e7da35c1d7940b481de08
acn-sbuad <stephanie.buadu@avanade.com>
2024-02-29 18:00:55 +0100

    shorter idle time

--- dbsetup.sh
+++ dbsetup.sh
@@ -6,8 +6,4 @@ psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb \
 -c "DO \$\$
     BEGIN CREATE ROLE platform_notifications WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-    END \$\$;
-
-    ALTER SYSTEM SET max_connections = 200;"
-
-
+    END \$\$;"
