119a5b0e754640bae72d9d8dcdbee12af9856df2
acn-sbuad <stephanie.buadu@avanade.com>
2024-02-29 17:57:41 +0100

    setting max connections in setup script

--- dbsetup.sh
+++ dbsetup.sh
@@ -6,4 +6,8 @@ psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb \
 -c "DO \$\$
     BEGIN CREATE ROLE platform_notifications WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-    END \$\$;"
+    END \$\$;
+
+    ALTER SYSTEM SET max_connections = 200;"
+
+
