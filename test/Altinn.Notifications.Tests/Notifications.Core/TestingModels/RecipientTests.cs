0be1ac0d330de6a8fa4b91001501c3ef958f88d6
acn-sbuad <stephanie.buadu@avanade.com>
2024-04-11 16:17:09 +0200

    added unit test

---
+++ test/Altinn.Notifications.Tests/Notifications.Core/TestingModels/RecipientTests.cs
@@ -0,0 +1,29 @@
+﻿using Altinn.Notifications.Core.Models;
+using Altinn.Notifications.Core.Models.Address;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.Core.TestingModels
+{
+    public class RecipientTests
+    {
+        [Fact]
+        public void DeepCopy_ReturnsNewInstance_ObjectsAreEquivalent()
+        {
+            // Arrange
+            var recipient = new Recipient()
+            {
+                NationalIdentityNumber = "16069412345",
+                IsReserved = true,
+                AddressInfo = [new SmsAddressPoint("+4781549300"), new EmailAddressPoint("test@digdir.no")]
+            };
+
+            // Act
+            var result = recipient.DeepCopy();
+
+            // Assert
+            Assert.NotSame(recipient, result);
+            Assert.Equivalent(recipient, result);
+        }
+    }
+}
