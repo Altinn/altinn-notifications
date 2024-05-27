f6cfac3e33a9da89c5c52638e0571fc8f6ce4d56
acn-sbuad <stephanie.buadu@avanade.com>
2024-05-27 12:30:07 +0200

    added required to sms model

--- src/Altinn.Notifications/Models/SmsNotificationOrderRequestExt.cs
+++ src/Altinn.Notifications/Models/SmsNotificationOrderRequestExt.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Altinn.Notifications.Models;
@@ -21,6 +22,7 @@ public class SmsNotificationOrderRequestExt : NotificationOrderRequestBaseExt
     /// Gets or sets the body of the SMS
     /// </summary>
     [JsonPropertyName("body")]
+    [Required]
     public string Body { get; set; } = string.Empty;
 
     /// <summary>
