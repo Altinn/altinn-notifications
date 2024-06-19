41f517b43fb6e3f3ada709fad0bac38c36146172
acn-sbuad <stephanie.buadu@avanade.com>
2024-06-19 09:56:20 +0200

    removed cathing of exception

--- src/Altinn.Notifications/Extensions/DateTimeExtensions.cs
+++ src/Altinn.Notifications/Extensions/DateTimeExtensions.cs
@@ -14,37 +14,25 @@ namespace Altinn.Notifications.Extensions
         /// </summary>
         /// <param name="dateTime">The date time object to check.</param>
         /// <returns>A boolean indicating whether it is within business hours or not.</returns>
-        /// <exception cref="InvalidOperationException">Thrown is any issues arise with time zone processing.</exception>
         public static bool IsWithinBusinessHours(this DateTime dateTime)
         {
-            try
+            DateTime dateTimeUtc = dateTime.Kind switch
             {
-                DateTime dateTimeUtc = dateTime.Kind switch
-                {
-                    DateTimeKind.Local => dateTime.ToUniversalTime(),
-                    DateTimeKind.Unspecified => DateTime.SpecifyKind(dateTime, DateTimeKind.Utc),
-                    _ => dateTime
-                };
+                DateTimeKind.Local => dateTime.ToUniversalTime(),
+                DateTimeKind.Unspecified => DateTime.SpecifyKind(dateTime, DateTimeKind.Utc),
+                _ => dateTime
+            };
 
-                TimeZoneInfo norwayTimeZone = TimeZoneInfo.FindSystemTimeZoneById(NorwayTimeZoneId);
+            TimeZoneInfo norwayTimeZone = TimeZoneInfo.FindSystemTimeZoneById(NorwayTimeZoneId);
 
-                DateTime norwayTime = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc, norwayTimeZone);
+            DateTime norwayTime = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc, norwayTimeZone);
 
-                TimeSpan startTime = new TimeSpan(GeneralSettings.SmsSendWindowStartHour, 0, 0);
-                TimeSpan endTime = new TimeSpan(GeneralSettings.SmsSendWindowEndHour, 0, 0);
+            TimeSpan startTime = new TimeSpan(GeneralSettings.SmsSendWindowStartHour, 0, 0);
+            TimeSpan endTime = new TimeSpan(GeneralSettings.SmsSendWindowEndHour, 0, 0);
 
-                TimeSpan currentTime = norwayTime.TimeOfDay;
+            TimeSpan currentTime = norwayTime.TimeOfDay;
 
-                return startTime < currentTime && currentTime < endTime;
-            }
-            catch (TimeZoneNotFoundException)
-            {
-                throw new InvalidOperationException($"The time zone ID '{NorwayTimeZoneId}' was not found on the local system.");
-            }
-            catch (InvalidTimeZoneException)
-            {
-                throw new InvalidOperationException($"The time zone ID '{NorwayTimeZoneId}' is invalid.");
-            }
+            return startTime < currentTime && currentTime < endTime;
         }
     }
 }
