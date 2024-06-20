d6fe532f921b7ae927dd32cd1ba40694131edf3b
Khan Mohammad R. <codemascot@hotmail.com>
2024-06-20 11:58:05 +0200

    Unnecessary tests removed form the organization test

--- test/k6/src/tests/orders_org_no.js
+++ test/k6/src/tests/orders_org_no.js
@@ -89,86 +89,8 @@ function TC01_PostEmailNotificationOrderRequest(data) {
   return selfLink;
 }
 
-// 02 - GET notification order by id
-function TC02_GetNotificationOrderById(data, selfLink, orderId) {
-  var response, success;
-  response = ordersApi.getByUrl(selfLink, data.token);
-  success = check(response, {
-    "GET notification order by id. Status is 200 OK": (r) => r.status === 200,
-  });
-
-  addErrorCount(success);
-  if (!success) {
-    // only continue to parse and check content if success response code
-    stopIterationOnFail(success);
-  }
-
-  success = check(JSON.parse(response.body), {
-    "GET notification order by id. Id property is a match": (order) =>
-      order.id === orderId,
-    "GET notification order by id. Creator property is a match": (order) =>
-      order.creator === "ttd",
-  });
-  addErrorCount(success);
-}
-
-// 03 - GET notification order by senders reference
-function TC03_GetNotificationOrderBySendersReference(data) {
-  var response, success;
-
-  response = ordersApi.getBySendersReference(data.sendersReference, data.token);
-  success = check(response, {
-    "GET notification order by senders reference. Status is 200 OK": (r) =>
-      r.status === 200,
-  });
-
-  addErrorCount(success);
-  if (!success) {
-    // only continue to parse and check content if success response code
-    stopIterationOnFail(success);
-  }
-
-  success = check(JSON.parse(response.body), {
-    "GET notification order by senders reference. Count is equal to 1": (
-      orderList
-    ) => orderList.count === 1,
-    "GET notification order by senders reference. Orderlist contains one element":
-      (orderList) =>
-        Array.isArray(orderList.orders) && orderList.orders.length == 1,
-  });
-}
-
-// 04 - GET notification order with status
-function TC04_GetNotificationOrderWithStatus(data, orderId) {
-  var response, success;
-
-  response = ordersApi.getWithStatus(orderId, data.token);
-  success = check(response, {
-    "GET notification order with status. Status is 200 OK": (r) =>
-      r.status === 200,
-  });
-
-  addErrorCount(success);
-  if (!success) {
-    // only continue to parse and check content if success response code
-    stopIterationOnFail(success);
-  }
-
-  success = check(JSON.parse(response.body), {
-    "GET notification order with status. Id property is a match": (order) =>
-      order.id === orderId,
-    "GET notification order with status. NotificationChannel is email": (
-      order
-    ) => order.notificationChannel === "Email",
-    "GET notification order with status. ProcessingStatus is defined": (
-      order
-    ) => order.processingStatus,
-  });
-  addErrorCount(success);
-}
-
-// 05 - GET email notification summary
-function TC05_GetEmailNotificationSummary(data, orderId) {
+// 02 - GET email notification summary
+function TC02_GetEmailNotificationSummary(data, orderId) {
   var response, success;
 
   response = notificationsApi.getEmailNotifications(orderId, data.token);
@@ -189,8 +111,8 @@ function TC05_GetEmailNotificationSummary(data, orderId) {
   });
 }
 
-// 06 - GET email notifications summary again after one minute for verification
-function TC06_GetEmailNotificationSummaryAgainAfterOneMinuteForVerification(data, orderId) {
+// 03 - GET email notifications summary again after one minute for verification
+function TC03_GetEmailNotificationSummaryAgainAfterOneMinuteForVerification(data, orderId) {
   sleep(60); // Waiting 1 minute for the notifications to be generated
   var response, success;
   response = notificationsApi.getEmailNotifications(orderId, data.token);
@@ -225,27 +147,21 @@ function TC06_GetEmailNotificationSummaryAgainAfterOneMinuteForVerification(data
 
 /*
  * 01 - POST email notification order request
- * 02 - GET notification order by id
- * 03 - GET notification order by senders reference
- * 04 - GET notification order with status
- * 05 - GET email notification summary
- * 06 - GET email notifications summary again after one minute for verification
+ * 02 - GET email notification summary
+ * 03 - GET email notifications summary again after one minute for verification
  */
 export default function (data) {
   try {
     if (data.runFullTestSet) {
       var selfLink = TC01_PostEmailNotificationOrderRequest(data);
       let id = selfLink.split("/").pop();
-      TC02_GetNotificationOrderById(data, selfLink, id);
-      TC03_GetNotificationOrderBySendersReference(data);
-      TC04_GetNotificationOrderWithStatus(data, id);
-      TC05_GetEmailNotificationSummary(data, id);
-      TC06_GetEmailNotificationSummaryAgainAfterOneMinuteForVerification(data, id);
+      TC02_GetEmailNotificationSummary(data, id);
+      TC03_GetEmailNotificationSummaryAgainAfterOneMinuteForVerification(data, id);
     } else {
       // Limited test set for use case tests
       var selfLink = TC01_PostEmailNotificationOrderRequest(data);
       let id = selfLink.split("/").pop();
-      TC05_GetEmailNotificationSummary(data, id);
+      TC02_GetEmailNotificationSummary(data, id);
     }
   } catch (error) {
     addErrorCount(false);
