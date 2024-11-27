7c6b4277168f77d286755e7844e86d8f7d2413e2
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2024-11-27 12:01:43 +0100

    Update two functions to retrieve recipient number and recipient name

--- src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getemailsstatusnewupdatestatus.sql
+++ src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getemailsstatusnewupdatestatus.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION notifications.getemails_statusnew_updatestatus()
-    RETURNS TABLE(alternateid uuid, subject text, body text, fromaddress text, toaddress text, contenttype text) 
+    RETURNS TABLE(alternateid uuid, subject text, body text, fromaddress text, toaddress text, contenttype text, recipientorgno text, recipientnin text) 
     LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
@@ -8,7 +8,7 @@ BEGIN
     SELECT emaillimittimeout INTO latest_email_timeout FROM notifications.resourcelimitlog WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
 		IF latest_email_timeout IS NOT NULL THEN
 			IF latest_email_timeout >= NOW() THEN
-				RETURN QUERY SELECT NULL::uuid AS alternateid, NULL::text AS subject, NULL::text AS body, NULL::text AS fromaddress, NULL::text AS toaddress, NULL::text AS contenttype WHERE FALSE;
+				RETURN QUERY SELECT NULL::uuid AS alternateid, NULL::text AS subject, NULL::text AS body, NULL::text AS fromaddress, NULL::text AS toaddress, NULL::text AS contenttype, NULL::text AS recipientorgno, NULL::text AS recipientnin WHERE FALSE;
                 RETURN;
 			ELSE 
 				UPDATE notifications.resourcelimitlog SET emaillimittimeout = NULL WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
@@ -20,8 +20,8 @@ BEGIN
                 UPDATE notifications.emailnotifications
                     SET result = 'Sending', resulttime = now()
                     WHERE result = 'New' 
-                    RETURNING notifications.emailnotifications.alternateid, _orderid, notifications.emailnotifications.toaddress)
-            SELECT u.alternateid, et.subject, et.body, et.fromaddress, u.toaddress, et.contenttype 
+                    RETURNING notifications.emailnotifications.alternateid, _orderid, notifications.emailnotifications.toaddress, notifications.emailnotifications.recipientorgno, notifications.emailnotifications.recipientnin)
+            SELECT u.alternateid, et.subject, et.body, et.fromaddress, u.toaddress, et.contenttype, u.recipientorgno, u.recipientnin
             FROM updated u, notifications.emailtexts et
             WHERE u._orderid = et._orderid;    
 END;
--- src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getsmsstatusnewupdatestatus.sql
+++ src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getsmsstatusnewupdatestatus.sql
@@ -1,17 +1,16 @@
 CREATE OR REPLACE FUNCTION notifications.getsms_statusnew_updatestatus()
-    RETURNS TABLE(alternateid uuid, sendernumber text, mobilenumber text, body text) 
+    RETURNS TABLE(alternateid uuid, sendernumber text, mobilenumber text, body text, recipientorgno text, recipientnin text) 
     LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
-		
     RETURN query 
     WITH updated AS (
         UPDATE notifications.smsnotifications
         SET result = 'Sending', resulttime = now()
         WHERE result = 'New' 
-        RETURNING notifications.smsnotifications.alternateid, _orderid, notifications.smsnotifications.mobilenumber)
-    SELECT u.alternateid, st.sendernumber, u.mobilenumber, st.body
+        RETURNING notifications.smsnotifications.alternateid, _orderid, notifications.smsnotifications.mobilenumber, notifications.smsnotifications.recipientorgno, notifications.smsnotifications.recipientnin)
+    SELECT u.alternateid, st.sendernumber, u.mobilenumber, st.body, u.recipientorgno, u.recipientnin
     FROM updated u, notifications.smstexts st
-    WHERE u._orderid = st._orderid;    
+    WHERE u._orderid = st._orderid;        
 END;
 $BODY$;
