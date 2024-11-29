e0d494cda72143abf7413e21dde7bfa7c2a23390
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2024-11-29 08:03:29 +0100

    Use SELECT INTO for assigning values to the __orderid variable and improve indentation for better readability.

--- src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/insertsmsnotification.sql
+++ src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/insertsmsnotification.sql
@@ -1,43 +1,45 @@
 CREATE OR REPLACE PROCEDURE notifications.insertsmsnotification(
-_orderid uuid, 
-_alternateid uuid, 
-_recipientorgno TEXT, 
-_recipientnin TEXT,
-_mobilenumber TEXT, 
-_customizedbody TEXT,
-_result text, 
-_smscount integer,
-_resulttime timestamptz, 
-_expirytime timestamptz
+    _orderid uuid, 
+    _alternateid uuid, 
+    _recipientorgno TEXT, 
+    _recipientnin TEXT,
+    _mobilenumber TEXT, 
+    _customizedbody TEXT,
+    _result text, 
+    _smscount integer,
+    _resulttime timestamptz, 
+    _expirytime timestamptz
 )
 LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
-__orderid BIGINT := (SELECT _id from notifications.orders
-			where alternateid = _orderid);
+    __orderid BIGINT;
 BEGIN
+    SELECT _id INTO __orderid 
+    FROM notifications.orders
+    WHERE alternateid = _orderid;
 
-INSERT INTO notifications.smsnotifications(
-_orderid, 
-alternateid,
-recipientorgno, 
-recipientnin, 
-mobilenumber,
-customizedbody,
-result,
-smscount,
-resulttime,
-expirytime)
-VALUES (
-__orderid,
-_alternateid,
-_recipientorgno, 
-_recipientnin, 
-_mobilenumber,
-_customizedbody,
-_result::smsnotificationresulttype,
-_smscount,
-_resulttime,
-_expirytime);
+    INSERT INTO notifications.smsnotifications(
+        _orderid, 
+        alternateid,
+        recipientorgno, 
+        recipientnin, 
+        mobilenumber,
+        customizedbody,
+        result,
+        smscount,
+        resulttime,
+        expirytime)
+    VALUES (
+        __orderid,
+        _alternateid,
+        _recipientorgno, 
+        _recipientnin, 
+        _mobilenumber,
+        _customizedbody,
+        _result::smsnotificationresulttype,
+        _smscount,
+        _resulttime,
+        _expirytime);
 END;
 $BODY$;
