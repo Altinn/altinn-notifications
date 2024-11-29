d85694467c7d4ebdb1afeacba7aac0125bcc5088
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2024-11-29 07:57:38 +0100

    Use explicit JOIN instead of the implicit join in the FROM clause and ensure consistent indentation for better readability.

--- src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getsmsstatusnewupdatestatus.sql
+++ src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getsmsstatusnewupdatestatus.sql
@@ -1,16 +1,23 @@
 CREATE OR REPLACE FUNCTION notifications.getsms_statusnew_updatestatus()
-    RETURNS TABLE(alternateid uuid, sendernumber text, mobilenumber text, body text, recipientorgno text, recipientnin text) 
+    RETURNS TABLE(alternateid uuid, sendernumber text, mobilenumber text, body text) 
     LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
-    RETURN query 
+    RETURN QUERY 
     WITH updated AS (
         UPDATE notifications.smsnotifications
         SET result = 'Sending', resulttime = now()
         WHERE result = 'New' 
-        RETURNING notifications.smsnotifications.alternateid, _orderid, notifications.smsnotifications.mobilenumber, notifications.smsnotifications.recipientorgno, notifications.smsnotifications.recipientnin)
-    SELECT u.alternateid, st.sendernumber, u.mobilenumber, CASE WHEN u.customizedbody IS NOT NULL AND u.customizedbody <> '' THEN u.customizedbody ELSE st.body END AS body, u.recipientorgno, u.recipientnin
-    FROM updated u, notifications.smstexts st
-    WHERE u._orderid = st._orderid;        
+        RETURNING notifications.smsnotifications.alternateid, 
+                  _orderid, 
+                  notifications.smsnotifications.mobilenumber,
+                  notifications.smsnotifications.customizedbody
+    )
+    SELECT u.alternateid, 
+           st.sendernumber, 
+           u.mobilenumber, 
+           CASE WHEN u.customizedbody IS NOT NULL AND u.customizedbody <> '' THEN u.customizedbody ELSE st.body END AS body
+    FROM updated u
+    JOIN notifications.smstexts st ON u._orderid = st._orderid;        
 END;
 $BODY$;
