3763f01ce7ee5ff99f6b2cf33533eb0dfabe6fd5
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2024-11-29 07:53:29 +0100

    Use explicit JOIN instead of the implicit join in the FROM clause and improve indentation for better readability.

--- src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getemailsstatusnewupdatestatus.sql
+++ src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/getemailsstatusnewupdatestatus.sql
@@ -5,24 +5,47 @@ AS $BODY$
 DECLARE
     latest_email_timeout TIMESTAMP WITH TIME ZONE;
 BEGIN
-    SELECT emaillimittimeout INTO latest_email_timeout FROM notifications.resourcelimitlog WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
-		IF latest_email_timeout IS NOT NULL THEN
-			IF latest_email_timeout >= NOW() THEN
-				RETURN QUERY SELECT NULL::uuid AS alternateid, NULL::text AS subject, NULL::text AS body, NULL::text AS fromaddress, NULL::text AS toaddress, NULL::text AS contenttype WHERE FALSE;
-                RETURN;
-			ELSE 
-				UPDATE notifications.resourcelimitlog SET emaillimittimeout = NULL WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
-			END IF;
-		END IF;
-		
-        RETURN query 
-            WITH updated AS (
-                UPDATE notifications.emailnotifications
-                    SET result = 'Sending', resulttime = now()
-                    WHERE result = 'New' 
-                    RETURNING notifications.emailnotifications.alternateid, _orderid, notifications.emailnotifications.toaddress, notifications.emailnotifications.customizedsubject, notifications.emailnotifications.customizedbody)
-            SELECT u.alternateid, CASE WHEN u.customizedsubject IS NOT NULL AND u.customizedsubject <> '' THEN u.customizedsubject ELSE et.subject END AS subject, CASE WHEN u.customizedbody IS NOT NULL AND u.customizedbody <> '' THEN u.customizedbody ELSE et.body END AS body, et.fromaddress, u.toaddress, et.contenttype 
-            FROM updated u, notifications.emailtexts et
-            WHERE u._orderid = et._orderid;    
+    SELECT emaillimittimeout 
+    INTO latest_email_timeout 
+    FROM notifications.resourcelimitlog 
+    WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
+
+    IF latest_email_timeout IS NOT NULL THEN
+        IF latest_email_timeout >= NOW() THEN
+            RETURN QUERY 
+            SELECT NULL::uuid AS alternateid, 
+                   NULL::text AS subject, 
+                   NULL::text AS body, 
+                   NULL::text AS fromaddress, 
+                   NULL::text AS toaddress, 
+                   NULL::text AS contenttype 
+            WHERE FALSE;
+            RETURN;
+        ELSE 
+            UPDATE notifications.resourcelimitlog 
+            SET emaillimittimeout = NULL 
+            WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);
+        END IF;
+    END IF;
+    
+    RETURN QUERY 
+    WITH updated AS (
+        UPDATE notifications.emailnotifications
+        SET result = 'Sending', resulttime = now()
+        WHERE result = 'New' 
+        RETURNING notifications.emailnotifications.alternateid, 
+                  _orderid, 
+                  notifications.emailnotifications.toaddress, 
+                  notifications.emailnotifications.customizedsubject, 
+                  notifications.emailnotifications.customizedbody
+    )
+    SELECT u.alternateid, 
+           CASE WHEN u.customizedsubject IS NOT NULL AND u.customizedsubject <> '' THEN u.customizedsubject ELSE et.subject END AS subject, 
+           CASE WHEN u.customizedbody IS NOT NULL AND u.customizedbody <> '' THEN u.customizedbody ELSE et.body END AS body, 
+           et.fromaddress, 
+           u.toaddress, 
+           et.contenttype 
+    FROM updated u
+    JOIN notifications.emailtexts et ON u._orderid = et._orderid;    
 END;
 $BODY$;
