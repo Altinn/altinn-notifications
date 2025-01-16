a168797c7e04e3b0fd33f257929f6e351f611de3
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-16 14:56:06 +0100

    Modernizing the logic and improve readability.

--- test/k6/src/apiHelpers.js
+++ test/k6/src/apiHelpers.js
@@ -1,61 +1,53 @@
 /**
  * Build a string in a format of query param to the endpoint
- * @param {*} queryparams a json object with key as query name and value as query value
+ * @param {*} queryparams a JSON object with key as query name and value as query value
  * @example {"key1": "value1", "key2": "value2"}
  * @returns string a string like key1=value&key2=value2
  */
 export function buildQueryParametersForEndpoint(queryparams) {
-  var query = "?";
-  Object.keys(queryparams).forEach(function (key) {
+  let query = "?";
+  Object.keys(queryparams).forEach((key) => {
     if (Array.isArray(queryparams[key])) {
       queryparams[key].forEach((value) => {
-        query += key + "=" + value + "&";
+        query += `${key}=${value}&`;
       });
     } else {
-      query += key + "=" + queryparams[key] + "&";
+      query += `${key}=${queryparams[key]}&`;
     }
   });
   query = query.slice(0, -1);
   return query;
 }
 
-export function buildHeaderWithBearer(token){
-    var params = {
-        headers: {
-          Authorization: "Bearer " + token
-        }
-      };
-
-      return params;
+export function buildHeaderWithBearer(token) {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
 }
 
-export function buildHeaderWithBasic(token){
-  var params = {
-      headers: {
-        Authorization: "Basic " + token
-      }
-    };
-
-    return params;
+export function buildHeaderWithBasic(token) {
+  return {
+    headers: {
+      Authorization: `Basic ${token}`
+    }
+  };
 }
 
 export function buildHeaderWithBearerAndContentType(token) {
-  var params = {
+  return {
     headers: {
-      Authorization: "Bearer " + token,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json"
     }
   };
-
-  return params;
 }
 
 export function buildHeaderWithContentType(contentType) {
-  var params = {
+  return {
     headers: {
       "Content-Type": contentType
     }
   };
-
-  return params;
 }
