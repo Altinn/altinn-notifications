ab97d6352fdf29edb1d1d97b803d14764b3135f7
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-16 14:58:36 +0100

    Modernizing the logic and simplified the return statements.

--- test/k6/src/api/notifications/notifications.js
+++ test/k6/src/api/notifications/notifications.js
@@ -1,19 +1,15 @@
 import http from "k6/http";
-
 import * as config from "../../config.js";
-
 import * as apiHelpers from "../../apiHelpers.js";
 
 export function getEmailNotifications(orderId, token) {
-  var endpoint = config.notifications.notifications_email(orderId);
-  var params = apiHelpers.buildHeaderWithBearer(token);
-  var response = http.get(endpoint, params);
-  return response;
+  const params = apiHelpers.buildHeaderWithBearer(token);
+  const endpoint = config.notifications.notifications_email(orderId);
+  return http.get(endpoint, params);
 }
 
 export function getSmsNotifications(orderId, token) {
-  var endpoint = config.notifications.notifications_sms(orderId);
-  var params = apiHelpers.buildHeaderWithBearer(token);
-  var response = http.get(endpoint, params);
-  return response;
+  const params = apiHelpers.buildHeaderWithBearer(token);
+  const endpoint = config.notifications.notifications_sms(orderId);
+  return http.get(endpoint, params);
 }
