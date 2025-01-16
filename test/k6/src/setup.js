6487ecefe8ade1066c79192f1ae2b92afba48e2b
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-16 15:48:19 +0100

    Modernized the logic.

--- test/k6/src/setup.js
+++ test/k6/src/setup.js
@@ -1,21 +1,22 @@
-import * as authentication from "./api/authentication.js";
 import * as maskinporten from "./api/maskinporten.js";
+import * as authentication from "./api/authentication.js";
 import * as tokenGenerator from "./api/token-generator.js";
 
 const environment = __ENV.env.toLowerCase();
 
 /*
- * generate an altinn token for org based on the environment using AltinnTestTools
- * If org is not provided TTD will be used.
- * @returns altinn token with the provided scopes for an org
+ * Generate an Altinn token for an organization based on the environment using AltinnTestTools.
+ * If organization is not provided, TTD will be used.
+ * @returns Altinn token with the provided scopes for an organization.
  */
 export function getAltinnTokenForOrg(scopes, org = "ttd", orgNo = "991825827") {
-  if ((environment == "prod" || environment == "tt02") && org == "ttd") {
-    var accessToken = maskinporten.generateAccessToken(scopes);
+  if ((environment === "prod" || environment === "tt02") && org === "ttd") {
+    const accessToken = maskinporten.generateAccessToken(scopes);
+
     return authentication.exchangeToAltinnToken(accessToken, true);
   }
 
-  var queryParams = {
+  const queryParams = {
     env: environment,
     scopes: scopes.replace(/ /gi, ","),
     org: org,
