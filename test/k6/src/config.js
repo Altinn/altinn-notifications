6df590a7db3a78c31e655e9e10567f200e3c92cc
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-01-16 14:52:44 +0100

    Remove the test environment at21 and modernizing the configuration logic.

--- test/k6/src/config.js
+++ test/k6/src/config.js
@@ -1,6 +1,5 @@
-// Baseurls for platform
+// Base URLs for platforms
 const baseUrls = {
-  at21: "at21.altinn.cloud",
   at22: "at22.altinn.cloud",
   at23: "at23.altinn.cloud",
   at24: "at24.altinn.cloud",
@@ -9,56 +8,42 @@ const baseUrls = {
 };
 
 const maskinportenBaseUrls = {
-  tt02: "https://test.maskinporten.no/",
   prod: "https://maskinporten.no/",
+  tt02: "https://test.maskinporten.no/"
 };
 
 // Get values from environment
 const environment = __ENV.env.toLowerCase();
-const baseUrl = baseUrls[environment];
-const maskinportenBaseUrl = maskinportenBaseUrls[environment];
 const subscriptionKey = __ENV.subscriptionKey;
 
+const baseUrl = baseUrls[environment];
+const maskinportenBaseUrl = maskinportenBaseUrls[environment];
+
 // AltinnTestTools
-export var tokenGenerator = {
+export const tokenGenerator = {
   getEnterpriseToken:
     "https://altinn-testtools-token-generator.azurewebsites.net/api/GetEnterpriseToken",
 };
 
 // Platform Notifications
-export var notifications = {
-  orders_email:
-    "https://platform." + baseUrl + "/notifications/api/v1/orders/email/",
-  orders_sms:
-    "https://platform." + baseUrl + "/notifications/api/v1/orders/sms/",
-  orders_status: function (orderId) {
-    return `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/status`;
-  },
-  orders_fromId: function (orderId) {
-    return `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}`;
-  },
-  orders_fromSendersRef: function (sendersReference) {
-    return `https://platform.${baseUrl}/notifications/api/v1/orders?sendersReference=${sendersReference}`;
-  },
-  notifications_email: function (orderId) {
-    return `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/notifications/email/`;
-  },
-  notifications_sms: function (orderId) {
-    return `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/notifications/sms/`;
-  },
-  conditionCheck: function(conditionMet) {
-    return `https://platform.${baseUrl}/notifications/api/v1/tests/sendcondition?conditionMet=${conditionMet}&subscription-key=${subscriptionKey}`;
-  }
+export const notifications = {
+  orders_sms: `https://platform.${baseUrl}/notifications/api/v1/orders/sms/`,
+  orders_email: `https://platform.${baseUrl}/notifications/api/v1/orders/email/`,
+  orders_fromId: (orderId) => `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}`,
+  orders_status: (orderId) => `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/status`,
+  notifications_sms: (orderId) => `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/notifications/sms/`,
+  notifications_email: (orderId) => `https://platform.${baseUrl}/notifications/api/v1/orders/${orderId}/notifications/email/`,
+  orders_fromSendersRef: (sendersReference) => `https://platform.${baseUrl}/notifications/api/v1/orders?sendersReference=${sendersReference}`,
+  conditionCheck: (conditionMet) => `https://platform.${baseUrl}/notifications/api/v1/tests/sendcondition?conditionMet=${conditionMet}&subscription-key=${subscriptionKey}`
 };
 
 // Platform Authentication
-export var platformAuthentication = {
-  exchange:
-    "https://platform." + baseUrl + "/authentication/api/v1/exchange/maskinporten",
+export const platformAuthentication = {
+  exchange: `https://platform.${baseUrl}/authentication/api/v1/exchange/maskinporten`,
 };
 
 // Maskinporten
-export var maskinporten = {
+export const maskinporten = {
   audience: maskinportenBaseUrl,
-  token: maskinportenBaseUrl + "token",
+  token: `${maskinportenBaseUrl}token`,
 };
