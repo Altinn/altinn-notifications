984e57bd13f91b061dfcd2a91f21b52bbaf621cf
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-02-12 13:26:39 +0100

    Remove two nullable directives.

--- src/Altinn.Notifications.Core/Models/Notification/SmsSendOperationResult.cs
+++ src/Altinn.Notifications.Core/Models/Notification/SmsSendOperationResult.cs
@@ -1,6 +1,4 @@
-﻿#nullable enable
-
-using System.Text.Json;
+﻿using System.Text.Json;
 
 using Altinn.Notifications.Core.Enums;
 
--- src/Altinn.Notifications.Core/Models/Recipients/SmsRecipient.cs
+++ src/Altinn.Notifications.Core/Models/Recipients/SmsRecipient.cs
@@ -1,5 +1,3 @@
-#nullable enable
-
 namespace Altinn.Notifications.Core.Models.Recipients;
 
 /// <summary>
