7b635f2d534784d510177d80f535aa6d609b3ea1
olebhansen <951969+olebhansen@users.noreply.github.com>
2025-02-19 12:46:29 +0100

    Adding basic state to demo idempotency. Changed to 201 + 200 response code semantics

--- src/Altinn.Notifications.NewApiDemo/Program.cs
+++ src/Altinn.Notifications.NewApiDemo/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebApplication1;
 using Scalar.AspNetCore;
 using System.ComponentModel;
+using System.Runtime.InteropServices.JavaScript;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.OpenApi.Models;
@@ -55,11 +56,45 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+Dictionary<string, Guid> idempotencyToOrderRepo = new Dictionary<string, Guid>();
+Dictionary<Guid, NotificationOrderResponse> orderRepo = new Dictionary<Guid, NotificationOrderResponse>();
+//Dictionary<Guid, Object> outboundMessageRepo = new Dictionary<Guid, Object>();
+
+app.MapGet("/order/{notificationOrderId}",
+    ([FromRoute] Guid notificationOrderId) =>
+    {
+        NotificationOrderResponse response;
+
+        if (orderRepo.TryGetValue(notificationOrderId, out response))
+        {
+            return Results.Ok(response);
+        }
+
+        return Results.NotFound();
+    });
 
 app.MapPost("/order",
         ([FromBody] Notification notification) =>
         {
             //validation
+            
+            //known idempotencyId?
+            
+            Guid orderId;
+            if (idempotencyToOrderRepo.TryGetValue(notification.IdempotencyId, out orderId))
+            {
+                NotificationOrderResponse existingResponse;
+                if (orderRepo.TryGetValue(orderId, out existingResponse)){
+                    return Results.Ok(existingResponse);
+                }
+                else
+                {
+                    return Results.InternalServerError(
+                        "Inconsistent data-store state. IdempotencyId not found in idempotencyToOrderRepo, but order not found in orderRepo");
+                }
+            }
+            
+            //TODO: convert/normalize to outbound message
             
             NotificationOrderResponse response = new()
             {
@@ -79,6 +114,11 @@ app.MapPost("/order",
                 
                 }
             };
+            
+            idempotencyToOrderRepo.Add(notification.IdempotencyId, response.NotificationOrderId);
+            orderRepo.Add(response.NotificationOrderId, response);
+           //TODO: add outbound messages to repo
+            
 
             DateTime plannedSendTime = notification.RequestedSendTime ?? DateTime.Now;
 
@@ -87,10 +127,11 @@ app.MapPost("/order",
             (notification.Reminders ?? new List<Reminder>()).ForEach(r => Console.Out.WriteLine("Planning reminder {0} for {1}", r.SendersReference, plannedSendTime.AddDays(r.RequestedSendTimeDelayDays ?? 0)));
                 
             
-            return Results.Ok(response);
+            return Results.Created(string.Format("/order/{0}", response.NotificationOrderId ), response);
         })
     //.Accepts<Notification>("application/json") //json is default
     .Produces<NotificationOrderResponse>(StatusCodes.Status200OK)
+    .Produces<NotificationOrderResponse>(StatusCodes.Status201Created)
     .Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
     .WithName("CreateNotification")
     .WithSummary("Create a notification")
@@ -107,7 +148,8 @@ app.MapPost("/order",
     
     .WithOpenApi(operation =>
         {
-            operation.Responses["200"].Description = "The creation of the notification and all associated reminders were successfull. The response contains a list of all notifications and reminders created, with their respective ids.";
+            operation.Responses["200"].Description = "The creation of the notification and all associated reminders associated with the provided idempotencyId was *already* successfully carried out. The response contains a list of all notifications and reminders *that was previously* created, with their respective ids. A 200 OK response indicates that whatever was in the current payload was ignored, in favour of the already created items.";
+            operation.Responses["201"].Description = "The creation of the notification and all associated reminders was successful. The response contains a list of all notifications and reminders created, with their respective ids.";
             operation.Responses["400"].Description =
                 "The provided data was not parsable. NB: The ProblemDetails object returned in this call will most likely change!";
 
