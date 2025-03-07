366b15bdaed8a2b230a2be1e0b8628056fd88f87
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-03-07 16:26:04 +0100

    Create SMS settings object

--- src/Altinn.Notifications/Models/SmsTemplateExt.cs
+++ src/Altinn.Notifications/Models/SmsTemplateExt.cs
@@ -1,22 +1,23 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Altinn.Notifications.Models
+namespace Altinn.Notifications.Models;
+
+/// <summary>
+/// Template for an SMS notification
+/// </summary>
+public class SmsTemplateExt
 {
     /// <summary>
-    /// Template for an sms notification
+    /// Gets the number from which the SMS is created by the template    
     /// </summary>
-    public class SmsTemplateExt
-    {
-        /// <summary>
-        /// Gets the number from which the SMS is created by the template    
-        /// </summary>
-        [JsonPropertyName("senderNumber")]
-        public string SenderNumber { get; set; } = string.Empty;
+    [JsonPropertyName("senderNumber")]
+    [JsonPropertyOrder(1)]
+    public string SenderNumber { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Gets the body of SMSs created by the template    
-        /// </summary>
-        [JsonPropertyName("body")]
-        public string Body { get; set; } = string.Empty;
-    }
+    /// <summary>
+    /// Gets the body of SMSs created by the template    
+    /// </summary>
+    [JsonPropertyName("body")]
+    [JsonPropertyOrder(2)]
+    public string Body { get; set; } = string.Empty;
 }
---
+++ src/Altinn.Notifications/Models/SmsTemplateWithSendingTimePolicyExt.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Models;
+
+/// <summary>
+/// Represents an SMS notification template with an associated sending time policy.
+/// This class extends <see cref="SmsTemplateExt"/> by adding a policy that determines when the SMS should be sent.
+/// </summary>
+[JsonDerivedType(typeof(SmsTemplateWithSendingTimePolicyExt), "smsSettings")]
+public class SmsTemplateWithSendingTimePolicyExt : SmsTemplateExt
+{
+    /// <summary>
+    /// Gets or sets the sending time policy for when the SMS should be sent.
+    /// </summary>
+    [JsonPropertyOrder(3)]
+    [JsonPropertyName("sendingTimePolicy")]
+    public SendingTimePolicyExt SendingTimePolicy { get; set; } = SendingTimePolicyExt.Unrestricted;
+}
