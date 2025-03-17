c638ed10ab2ae5ea19430ef974712c430b42adc7
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-03-17 08:40:50 +0100

    Create a new data definition script to add a new table to the database schema.

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.37/02-new-table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE IF NOT EXISTS notifications.orderswithreminders
+(
+    _id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    orderid UUID NOT NULL,
+    idempotencyid TEXT NOT NULL,
+    creatorname TEXT NOT NULL,
+    created TIMESTAMPTZ NOT NULL,
+    processed TIMESTAMPTZ,
+    processedstatus orderprocessingstate DEFAULT 'Registered',
+    orderwithreminder JSONB NOT NULL,
+    CONSTRAINT orderswithreminders_unique_idempotencyid_creatorname UNIQUE (idempotencyid, creatorname)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_orderswithreminders_orderid ON notifications.orderswithreminders(orderid);
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE notifications.orderswithreminders TO platform_notifications;
