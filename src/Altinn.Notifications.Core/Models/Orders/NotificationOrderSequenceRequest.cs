9c0f31a22480b54f128d1df34402da81b7c1b529
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-03-21 11:54:55 +0100

    Add extra members to the server client model

--- src/Altinn.Notifications.Core/Models/Orders/NotificationOrderSequenceRequest.cs
+++ src/Altinn.Notifications.Core/Models/Orders/NotificationOrderSequenceRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace Altinn.Notifications.Core.Models.Orders;
+﻿using Altinn.Notifications.Core.Models.Recipients;
+
+namespace Altinn.Notifications.Core.Models.Orders;
 
 /// <summary>
 /// Represents a request to create a notification order with non or more reminders.
@@ -19,4 +21,22 @@ public class NotificationOrderSequenceRequest : NotificationOrderScheduling
     /// Gets or sets the idempotency identifier defined by the sender.
     /// </summary>
     public required string IdempotencyId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the required recipient information for this reminder.
+    /// </summary>
+    /// <remarks>
+    /// Specifies the target recipient through one of the supported channels:
+    /// email address, SMS number, national identity number, or organization number.
+    /// The reminder can be directed to a different recipient than the initial notification.
+    /// </remarks>
+    public required RecipientSpecification Recipient { get; set; }
+
+    /// <summary>
+    /// Gets or sets a list of reminders that may be triggered under certain conditions after the initial notification has been processed.
+    /// </summary>
+    /// <remarks>
+    /// Each reminder can have its own recipient settings, delay period, and triggering conditions.
+    /// </remarks>
+    public List<NotificationReminder>? Reminders { get; set; }
 }
