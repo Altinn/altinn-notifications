8991b9d4a31a5ac43e7e0feb1085b4f9793dd28b
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-03-25 14:36:11 +0100

    Implement two unit tests to handle edge cases.

--- test/Altinn.Notifications.Tests/Notifications.Core/TestingServices/OrderRequestServiceTests.cs
+++ test/Altinn.Notifications.Tests/Notifications.Core/TestingServices/OrderRequestServiceTests.cs
@@ -564,9 +564,9 @@ public class OrderRequestServiceTests
         var orderRequest = new NotificationOrderChainRequest.NotificationOrderChainRequestBuilder()
             .SetOrderId(orderId)
             .SetCreator(new Creator("ttd"))
-            .SetSendersReference("senders-reference")
             .SetRequestedSendTime(currentTime.AddMinutes(5))
             .SetIdempotencyId("84CD3017-92E3-4C3D-80DE-C10338F30813")
+            .SetSendersReference("43F2C14A-62E4-4258-8887-3414296E7D82")
             .SetRecipient(new NotificationRecipient { RecipientEmail = new RecipientEmail { EmailAddress = "recipient@example.com", Settings = new EmailSendingOptions { Body = "Email body", Subject = "Email subject" } } })
             .SetReminders(
             [
@@ -575,7 +575,7 @@ public class OrderRequestServiceTests
                         DelayDays = 1,
                         Recipient = new NotificationRecipient { RecipientEmail = new RecipientEmail { EmailAddress = "reminder@example.com", Settings = new EmailSendingOptions { Body = "Reminder body", Subject = "Reminder subject" } } },
                         RequestedSendTime = currentTime.AddMinutes(20),
-                        SendersReference = "reminder-senders-reference"
+                        SendersReference = "DAFBD9F5-7CCA-468B-AD20-793BF44D9C44"
                     }
             ])
             .Build();
@@ -600,6 +600,91 @@ public class OrderRequestServiceTests
         Assert.Single(response.CreationResult.Reminders);
         Assert.Equal("DAFBD9F5-7CCA-468B-AD20-793BF44D9C44", response.CreationResult.Reminders[0].SendersReference);
 
+        repoMock.VerifyAll();
+    }
+
+    [Fact]
+    public async Task RegisterNotificationOrderChain_WithoutReminders_OrderChainCreated()
+    {
+        // Arrange
+        Guid orderId = Guid.NewGuid();
+        DateTime currentTime = DateTime.UtcNow;
+        var orderRequest = new NotificationOrderChainRequest.NotificationOrderChainRequestBuilder()
+            .SetOrderId(orderId)
+            .SetCreator(new Creator("ttd"))
+            .SetRequestedSendTime(currentTime.AddMinutes(5))
+            .SetIdempotencyId("65698F3A-7B27-478C-9E76-A190C34A8099")
+            .SetSendersReference("674DF57E-5344-4106-95FA-24E19126FBD8")
+            .SetRecipient(new NotificationRecipient { RecipientEmail = new RecipientEmail { EmailAddress = "recipient@example.com", Settings = new EmailSendingOptions { Body = "Email body", Subject = "Email subject" } } })
+            .Build();
+
+        var expectedMainOrder = new NotificationOrder(orderId, "674DF57E-5344-4106-95FA-24E19126FBD8", [], currentTime.AddMinutes(10), NotificationChannel.Email, new Creator("ttd"), currentTime, [], null, null, null);
+
+        var repoMock = new Mock<IOrderRepository>();
+        repoMock.Setup(r => r.Create(It.IsAny<NotificationOrderChainRequest>(), It.IsAny<NotificationOrder>(), It.IsAny<List<NotificationOrder>>()))
+            .ReturnsAsync([expectedMainOrder]);
+
+        var service = GetTestService(repoMock.Object, null, orderId, currentTime);
+
+        // Act
+        var response = await service.RegisterNotificationOrderChain(orderRequest);
+
+        // Assert
+        Assert.Equal(orderId, response.Id);
+        Assert.Equal("674DF57E-5344-4106-95FA-24E19126FBD8", response.CreationResult.SendersReference);
+        Assert.Null(response.CreationResult.Reminders);
+        repoMock.VerifyAll();
+    }
+
+    [Fact]
+    public async Task RegisterNotificationOrderChain_RepositoryReturnsNull_ThrowsException()
+    {
+        // Arrange
+        Guid orderId = Guid.NewGuid();
+        DateTime currentTime = DateTime.UtcNow;
+        var orderRequest = new NotificationOrderChainRequest.NotificationOrderChainRequestBuilder()
+            .SetOrderId(orderId)
+            .SetCreator(new Creator("ttd"))
+            .SetRequestedSendTime(currentTime.AddMinutes(10))
+            .SetIdempotencyId("9E032152-8A09-4887-9F9F-E56FE3FBC8C7")
+            .SetSendersReference("A6BFDADD-A3D1-476D-8F64-55574CDCADCC")
+            .SetRecipient(new NotificationRecipient { RecipientEmail = new RecipientEmail { EmailAddress = "test@example.com", Settings = new EmailSendingOptions { Body = "Test body", Subject = "Test subject" } } })
+            .Build();
+
+        var repoMock = new Mock<IOrderRepository>();
+        repoMock.Setup(r => r.Create(It.IsAny<NotificationOrderChainRequest>(), It.IsAny<NotificationOrder>(), It.IsAny<List<NotificationOrder>>()))
+            .ReturnsAsync([]);
+
+        var service = GetTestService(repoMock.Object, null, orderId, currentTime);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(() => service.RegisterNotificationOrderChain(orderRequest));
+        repoMock.VerifyAll();
+    }
+
+    [Fact]
+    public async Task RegisterNotificationOrderChain_RepositoryReturnsEmptyList_ThrowsException()
+    {
+        // Arrange
+        DateTime currentTime = DateTime.UtcNow;
+        Guid orderId = Guid.NewGuid();
+        var orderRequest = new NotificationOrderChainRequest.NotificationOrderChainRequestBuilder()
+            .SetOrderId(orderId)
+            .SetCreator(new Creator("ttd"))
+            .SetRequestedSendTime(currentTime.AddMinutes(10))
+            .SetIdempotencyId("5D69E05E-8BC7-4736-BADA-C6CB00ED8C0A")
+            .SetSendersReference("D340DC99-E5B0-4153-B56E-B3946E8D4AC4")
+            .SetRecipient(new NotificationRecipient { RecipientEmail = new RecipientEmail { EmailAddress = "test@example.com", Settings = new EmailSendingOptions { Body = "Test body", Subject = "Test subject" } } })
+            .Build();
+
+        var repoMock = new Mock<IOrderRepository>();
+        repoMock.Setup(r => r.Create(It.IsAny<NotificationOrderChainRequest>(), It.IsAny<NotificationOrder>(), It.IsAny<List<NotificationOrder>>()))
+            .ReturnsAsync([]);
+
+        var service = GetTestService(repoMock.Object, null, orderId, currentTime);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(() => service.RegisterNotificationOrderChain(orderRequest));
         repoMock.VerifyAll();
     }
 
