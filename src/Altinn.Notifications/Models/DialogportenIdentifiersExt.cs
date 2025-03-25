1f7668b1ae2af224cdc79387d17dc07317962819
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-03-25 16:43:31 +0100

    Add JsonPropertyName attributes

--- src/Altinn.Notifications/Models/DialogportenIdentifiersExt.cs
+++ src/Altinn.Notifications/Models/DialogportenIdentifiersExt.cs
@@ -1,4 +1,6 @@
-﻿namespace Altinn.Notifications.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Models;
 
 /// <summary>
 /// Represents unique identifiers for dialogs and transmissions within Dialogporten.
@@ -8,10 +10,12 @@ public class DialogportenIdentifiersExt
     /// <summary>
     /// Gets or sets the identifier for a specific dialog within Dialogporten.
     /// </summary>
+    [JsonPropertyName("dialogId")] 
     public string? DialogId { get; set; }
 
     /// <summary>
     /// Gets or sets the identifier for a specific transmission within Dialogporten.
     /// </summary>
+    [JsonPropertyName("transmissionId")] 
     public string? TransmissionId { get; set; }
 }
