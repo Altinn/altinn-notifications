0e40bf675989652c35a98ea62cc2db96e6e474c8
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-03-26 12:06:50 +0100

    Add notificationOrderBaseValidator

---
+++ src/Altinn.Notifications/Validators/NotificationOrderBaseValidator.cs
@@ -0,0 +1,22 @@
+﻿using Altinn.Notifications.Models;
+using FluentValidation;
+
+namespace Altinn.Notifications.Validators
+{
+    /// <summary>
+    /// Validator for <see cref="NotificationOrderBaseExt"/>.
+    /// </summary>
+    public class NotificationOrderBaseValidator : AbstractValidator<NotificationOrderBaseExt>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationOrderBaseValidator"/> class.
+        /// </summary>
+        public NotificationOrderBaseValidator()
+        {
+            RuleFor(option => option.RequestedSendTime)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(DateTime.UtcNow)
+                .WithMessage("RequestedSendTime is required and must be greater than or equal to now.");
+        }
+    }
+}
