947f19da31cde9d1f0149b01e2750cbaf65ce005
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-04-03 12:53:07 +0200

    Fix alter tables script to check if column already exists

--- src/Altinn.Notifications.Persistence/Migration/v0.38/01-alter-tables.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.38/01-alter-tables.sql
@@ -1,3 +1,14 @@
-ALTER TABLE IF EXISTS notifications.orders
-    ADD COLUMN sendingtimepolicy integer;
 
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1
+             FROM information_schema.columns
+             WHERE table_schema = 'notifications'
+               AND table_name   = 'orders'
+               AND column_name  = 'sendingtimepolicy') THEN
+    RAISE NOTICE 'Column "sendingtimepolicy" already exists in table "orders".';
+  ELSE
+    ALTER TABLE IF EXISTS notifications.orders
+    ADD COLUMN sendingtimepolicy integer;
+  END IF;
+END $$;
