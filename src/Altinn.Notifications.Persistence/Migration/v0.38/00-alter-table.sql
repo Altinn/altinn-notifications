3accef279132aaeb91e872321cb7122d714de81f
Ahmed-Ghanam <114151129+Ahmed-Ghanam@users.noreply.github.com>
2025-04-07 16:47:51 +0200

    Use `IF NOT EXISTS` When creating a constraint (#793)
    * Use IF NOT EXISTS when creating the constraint on a table
    
    * Rename a database script
    
    * Regenerate the functions and procedures script

--- src/Altinn.Notifications.Persistence/Migration/v0.38/00-alter-table.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.38/00-alter-table.sql
@@ -1,3 +1,12 @@
-﻿-- Add a unique constraint for idempotencyid and creatorname
-ALTER TABLE notifications.orderschain
-ADD CONSTRAINT orderschain_unique_idempotencyid_creatorname UNIQUE (idempotencyid, creatorname);
+﻿-- Add a unique constraint for idempotencyid and creatorname if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'orderschain_unique_idempotencyid_creatorname'
+    ) THEN
+        ALTER TABLE notifications.orderschain
+        ADD CONSTRAINT orderschain_unique_idempotencyid_creatorname UNIQUE (idempotencyid, creatorname);
+    END IF;
+END $$;
