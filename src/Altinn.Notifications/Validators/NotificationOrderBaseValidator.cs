d5667d49b87d221160273036b154177f4fb59ea2
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-04-10 21:47:09 +0200

    Add time zone validation

--- src/Altinn.Notifications/Validators/NotificationOrderBaseValidator.cs
+++ src/Altinn.Notifications/Validators/NotificationOrderBaseValidator.cs
@@ -15,9 +15,14 @@ namespace Altinn.Notifications.Validators
         public NotificationOrderBaseValidator()
         {
             RuleFor(option => option.RequestedSendTime)
-                .NotEmpty()
+                .Must(sendTime => sendTime.Kind != DateTimeKind.Unspecified)
+                .WithMessage("The requested send time value must have specified a time zone.")
                 .GreaterThanOrEqualTo(DateTime.UtcNow)
                 .WithMessage("RequestedSendTime is required and must be greater than or equal to now.");
+
+            RuleFor(option => option.ConditionEndpoint)
+                .Must(conditionEndpoint => conditionEndpoint == null || Uri.IsWellFormedUriString(conditionEndpoint.ToString(), UriKind.Absolute))
+                .WithMessage("ConditionEndpoint must be a valid absolute URI or null.");
         }
     }
 }
