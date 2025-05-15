0165cf67154c28641c6392815c08468e1e930ee3
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-05-15 13:20:34 +0200

    Add status feed table

---
+++ src/Altinn.Notifications.Persistence/Migration/v0.43/01-new-table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE IF NOT EXISTS notifications.statusfeed
+(
+  _id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  orderid BIGINT NOT NULL,
+  creatorname TEXT NOT NULL,
+  created TIMESTAMPTZ NOT NULL,
+  orderstatus JSONB NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_statusfeed_orderid ON notifications.statusfeed(orderid);
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE notifications.statusfeed TO platform_notifications;
