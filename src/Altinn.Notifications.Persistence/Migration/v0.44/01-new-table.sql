2e9b2c25b92353702e62f2a5a76ceba33ef23e69
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-05-21 11:48:27 +0200

    Add indexes

--- src/Altinn.Notifications.Persistence/Migration/v0.44/01-new-table.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.44/01-new-table.sql
@@ -11,6 +11,8 @@
 CREATE INDEX IF NOT EXISTS idx_statusfeed_orderid ON notifications.statusfeed(orderid);
 CREATE INDEX IF NOT EXISTS idx_statusfeed_creatorname ON notifications.statusfeed (creatorname);
 CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_sequencenumber_creatorname ON notifications.statusfeed (sequencenumber, creatorname);
+CREATE INDEX IF NOT EXISTS idx_statusfeed_created ON notifications.statusfeed (created);
+CREATE INDEX IF NOT EXISTS idx_statusfeed_creatorname_created  ON notifications.statusfeed (creatorname, created DESC);
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE notifications.statusfeed TO platform_notifications;
 GRANT USAGE, SELECT ON SEQUENCE notifications.statusfeed__id_seq TO platform_notifications;
