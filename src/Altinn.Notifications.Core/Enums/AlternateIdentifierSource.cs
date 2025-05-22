a3a465acbd1444a91f447c5e00f451a08fb0d98a
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-05-22 08:16:59 +0200

    Introduce a new enumeration type to define the source types for alternate identifiers used in notification-related operations.

---
+++ src/Altinn.Notifications.Core/Enums/AlternateIdentifierSource.cs
@@ -0,0 +1,22 @@
+﻿namespace Altinn.Notifications.Core.Enums;
+
+/// <summary>
+/// Defines the source type for an alternate identifier used in notification operations.
+/// </summary>
+public enum AlternateIdentifierSource
+{
+    /// <summary>
+    /// The identifier is associated with an SMS notification.
+    /// </summary>
+    Sms,
+
+    /// <summary>
+    /// The identifier is associated with an email notification.
+    /// </summary>
+    Email,
+
+    /// <summary>
+    /// The identifier is associated with an order.
+    /// </summary>
+    Order
+}
