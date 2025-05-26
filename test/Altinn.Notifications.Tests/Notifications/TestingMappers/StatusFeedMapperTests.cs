b8c9af4c285fd16f224f9c601662a9b1ce9ac8c1
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-05-26 14:06:29 +0200

    Add mapper test

--- test/Altinn.Notifications.Tests/Notifications/TestingMappers/StatusFeedMapperTests.cs
+++ test/Altinn.Notifications.Tests/Notifications/TestingMappers/StatusFeedMapperTests.cs
@@ -1,69 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Altinn.Notifications.Core.Models.Delivery;
 using Altinn.Notifications.Mappers;
 using Altinn.Notifications.Models.Delivery;
 using Altinn.Notifications.Tests.TestData;
-using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
-namespace Altinn.Notifications.Tests.Notifications.TestingMappers
+namespace Altinn.Notifications.Tests.Notifications.TestingMappers;
+
+public class StatusFeedMapperTests
 {
-    public class StatusFeedMapperTests
+    private readonly ILogger<StatusFeedMapperTests> _logger = NullLogger<StatusFeedMapperTests>.Instance;
+
+    [Fact]
+    public void MapToOrderStatusExtList_MapsCorrectly()
     {
-        [Fact]
-        public void MapToOrderStatusExtList_MapsCorrectly()
+        // Arrange
+        using var jsonDocument = System.Text.Json.JsonDocument.Parse(TestDataConstants.OrderStatusFeedTestOrderCompleted);
+        using var jsonDocument2 = System.Text.Json.JsonDocument.Parse(TestDataConstants.OrderStatusFeedTestOrderCompleted2);
+        var jsonElement = jsonDocument.RootElement.Clone();
+        var jsonElement2 = jsonDocument2.RootElement.Clone();
+
+        var statusFeeds = new List<StatusFeed>
         {
-            // Arrange
-            using var jsonDocument = System.Text.Json.JsonDocument.Parse(TestDataConstants.OrderStatusFeedTestOrderCompleted);
-            using var jsonDocument2 = System.Text.Json.JsonDocument.Parse(TestDataConstants.OrderStatusFeedTestOrderCompleted2);
-            var jsonElement = jsonDocument.RootElement.Clone();
-            var jsonElement2 = jsonDocument2.RootElement.Clone();
+            new StatusFeed { SequenceNumber = 1, OrderStatus = TestDataConstants.OrderStatusFeedTestOrderCompleted },
+            new StatusFeed { SequenceNumber = 2, OrderStatus = TestDataConstants.OrderStatusFeedTestOrderCompleted2 }
+        };
 
-            var statusFeeds = new List<StatusFeed>
-            {
-                new StatusFeed { SequenceNumber = 1, OrderStatus = TestDataConstants.OrderStatusFeedTestOrderCompleted },
-                new StatusFeed { SequenceNumber = 2, OrderStatus = TestDataConstants.OrderStatusFeedTestOrderCompleted2 }
-            };
-
-            var statusFeedExtList = new List<StatusFeedExt>
-            {
-                new StatusFeedExt
-                {
-                    SequenceNumber = 1,
-                    OrderStatus = jsonElement
-                },
-                new StatusFeedExt
-                {
-                    SequenceNumber = 2,
-                    OrderStatus = jsonElement2
-                }
-            };
-
-            // Act
-            var result = statusFeeds.MapToStatusFeedExtList(NullLogger<StatusFeedMapperTests>.Instance);
-            
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equivalent(statusFeedExtList, result);
-        }
-
-        [Fact]
-        public void MapToOrderStatusExt_WithInvalidJson_ReturnsEmptyJsonElement()
+        var statusFeedExtList = new List<StatusFeedExt>
         {
-            // Arrange
-            var statusFeeds = new List<StatusFeed> { new() { SequenceNumber = 1, OrderStatus = "Invalid JSON" } };
+            new StatusFeedExt
+            {
+                SequenceNumber = 1,
+                OrderStatus = jsonElement
+            },
+            new StatusFeedExt
+            {
+                SequenceNumber = 2,
+                OrderStatus = jsonElement2
+            }
+        };
 
-            // Act
-            var results = statusFeeds.MapToStatusFeedExtList(NullLogger<StatusFeedMapperTests>.Instance);
+        // Act
+        var result = statusFeeds.MapToStatusFeedExtList(_logger);
 
-            // Assert
-            Assert.NotNull(results);
-            var singleResult = Assert.Single(results);
-            Assert.Equal(1, singleResult.SequenceNumber);
-            Assert.Equal("{}", singleResult.OrderStatus.ToString()); // Check if the OrderStatus is an empty JSON object
-        }
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equivalent(statusFeedExtList, result);
+    }
+
+    [Fact]
+    public void MapToOrderStatusExt_WithInvalidJson_ReturnsEmptyJsonElement()
+    {
+        // Arrange
+        var statusFeeds = new List<StatusFeed> { new() { SequenceNumber = 1, OrderStatus = "Invalid JSON" } };
+
+        // Act
+        var results = statusFeeds.MapToStatusFeedExtList(_logger);
+
+        // Assert
+        Assert.NotNull(results);
+        var singleResult = Assert.Single(results);
+        Assert.Equal(1, singleResult.SequenceNumber);
+        Assert.Equal("{}", singleResult.OrderStatus.ToString()); // Check if the OrderStatus is an empty JSON object
+    }
+
+    [Fact]
+    public void MapToOrderStatusExt_WithEmptyInput_ReturnsEmptyList()
+    {
+        // Arrange
+        var statusFeeds = new List<StatusFeed>();
+
+        // Act
+        var results = statusFeeds.MapToStatusFeedExtList(_logger);
+
+        // Assert
+        Assert.NotNull(results);
+        Assert.Empty(results);
     }
 }
