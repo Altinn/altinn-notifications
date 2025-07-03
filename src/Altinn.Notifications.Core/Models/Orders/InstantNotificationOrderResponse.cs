bb6d3fe96e7b1788edcc7c9cf12a44f4038e0af2
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-03 13:04:22 +0200

    Implement simple response model

---
+++ src/Altinn.Notifications.Core/Models/Orders/InstantNotificationOrderResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Notifications.Core.Models.Orders;
+
+/// <summary>
+/// Represents the response model for a request to send an SMS notification immediately.
+/// </summary>
+public class InstantNotificationOrderResponse
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for notification order.
+    /// </summary>
+    public required Guid ShipmentId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sender's reference identifier.
+    /// </summary>
+    public string? SendersReference { get; set; }
+}
---
+++ src/Altinn.Notifications/Models/Orders/InstantNotificationOrderResponseExt.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Models.Orders;
+
+/// <summary>
+/// Represents the response model for a request to send an SMS notification immediately.
+/// </summary>
+public class InstantNotificationOrderResponseExt
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for notification order.
+    /// </summary>
+    [JsonPropertyName("shipmentId")]
+    public required Guid ShipmentId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sender's reference identifier.
+    /// </summary>
+    [JsonPropertyName("sendersReference")]
+    public string? SendersReference { get; set; }
+}
