8f4da50a739900802487342c5396137f97de149a
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-03 13:43:03 +0200

    Implement a data model to represent the payload sent to the SMS service.

---
+++ src/Altinn.Notifications.Core/Models/InstantSmsPayload.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Altinn.Notifications.Core.Models;
+
+/// <summary>
+/// Represents an internal payload for sending an SMS instantly via the Notifications SMS service.
+/// </summary>
+public record InstantSmsPayload
+{
+    /// <summary>
+    /// The identifier or name displayed as the sender on the recipient’s device.
+    /// </summary>
+    public required string Sender { get; init; }
+
+    /// <summary>
+    /// The text content of the SMS message.
+    /// </summary>
+    public required string Message { get; init; }
+
+    /// <summary>
+    /// The recipient’s phone number.
+    /// </summary>
+    public required string Recipient { get; init; }
+
+    /// <summary>
+    /// Time-to-live for SMS delivery attempts, in seconds.
+    /// </summary>
+    public int TimeToLive { get; init; }
+
+    /// <summary>
+    /// The unique identifier linking this SMS to the notification record.
+    /// </summary>
+    public Guid NotificationId { get; init; }
+
+    /// <summary>
+    /// Serializes the <see cref="Sms"/> object to a JSON string.
+    /// </summary>
+    /// <returns>A JSON string representation of the <see cref="Sms"/> object.</returns>
+    public string Serialize()
+    {
+        return JsonSerializer.Serialize(this, JsonSerializerOptionsProvider.Options);
+    }
+}
