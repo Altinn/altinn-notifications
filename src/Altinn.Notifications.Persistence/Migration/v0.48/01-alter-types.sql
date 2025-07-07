882be2418ba5eacccba3f88d52948feed650231c
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-07 10:38:43 +0200

    Rename the new order type from Instant Notification to Instant

--- src/Altinn.Notifications.Persistence/Migration/v0.48/01-alter-types.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.48/01-alter-types.sql
@@ -4,9 +4,9 @@ BEGIN
         SELECT 1
         FROM pg_type t
         JOIN pg_enum e ON t.oid = e.enumtypid
-        WHERE t.typname = 'notificationordertype' AND e.enumlabel = 'InstantNotification'
+        WHERE t.typname = 'notificationordertype' AND e.enumlabel = 'Instant'
     ) THEN
-        ALTER TYPE public.notificationordertype ADD VALUE 'InstantNotification';
+        ALTER TYPE public.notificationordertype ADD VALUE 'Instant';
     END IF;
 END;
 $$;
