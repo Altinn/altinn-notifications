4e1b1517b6fa42c0a5dfb1432ff54a33fc29d709
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-09 14:43:32 +0200

    Implement a new type to hold tracking information for an instant notification order

---
+++ src/Altinn.Notifications.Core/Models/Orders/InstantNotificationOrderTracking.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Notifications.Core.Models.Orders;
+
+/// <summary>
+/// Represents the response model for a request to send an SMS notification immediately.
+/// </summary>
+public record InstantNotificationOrderTracking
+{
+    /// <summary>
+    /// The unique identifier for the notification order chain.
+    /// </summary>
+    public required Guid OrderChainId { get; init; }
+
+    /// <summary>
+    /// The unique identifier for notification order.
+    /// </summary>
+    public required NotificationOrderChainShipment Notification { get; init; }
+}
