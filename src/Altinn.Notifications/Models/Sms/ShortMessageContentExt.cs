fcab07c553c80b2cf20f8ce8b8c9c1285187f478
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-10 12:39:47 +0200

    Implement a new type to hold content and sender information for an SMS

---
+++ src/Altinn.Notifications/Models/Sms/ShortMessageContentExt.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Notifications.Models.Sms;
+
+/// <summary>
+/// Represents the content and sender information for an SMS (Short Message Service) message.
+/// </summary>
+public class ShortMessageContentExt
+{
+    /// <summary>
+    /// The sender identifier displayed in the recipient's SMS message.
+    /// </summary>
+    public string? Sender { get; init; }
+
+    /// <summary>
+    /// The text content of the SMS message to be delivered to the recipient.
+    /// </summary>
+    public required string Message { get; init; }
+}
