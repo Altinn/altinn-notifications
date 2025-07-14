1b64431d41a159555e519fe11a079949eab44ab0
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-14 09:08:32 +0200

    Update the data type

--- src/Altinn.Notifications/Models/Sms/ShortMessageContentExt.cs
+++ src/Altinn.Notifications/Models/Sms/ShortMessageContentExt.cs
@@ -6,7 +6,7 @@ namespace Altinn.Notifications.Models.Sms;
 /// <summary>
 /// Represents the content and sender information for an SMS (Short Message Service) message.
 /// </summary>
-public class ShortMessageContentExt
+public record ShortMessageContentExt
 {
     /// <summary>
     /// The sender identifier displayed in the recipient's SMS message.
