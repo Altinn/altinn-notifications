334fb42cccf7b862189adb9877301a2c1919b1bd
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-15 15:22:00 +0200

    Code refactoring

--- src/Altinn.Notifications.Persistence/Repository/InstantOrderRepository.cs
+++ src/Altinn.Notifications.Persistence/Repository/InstantOrderRepository.cs
@@ -18,9 +18,9 @@ public class InstantOrderRepository : IInstantOrderRepository
 
     private const string _insertOrderSql = "select notifications.insertorder($1, $2, $3, $4, $5, $6, $7, $8, $9)"; // (_alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _notificationorder, _sendingtimepolicy, _type, _processingstatus)
     private const string _insertSmsTextSql = "insert into notifications.smstexts(_orderid, sendernumber, body) VALUES ($1, $2, $3)"; // (_orderid, _sendernumber, _body)
-    private const string _insertorderchainSql = "call notifications.insertorderchain($1, $2, $3, $4, $5)"; // (_orderid, _idempotencyid, _creatorname, _created, _orderchain)
-    private const string _getInstantOrderTrackingSql = "SELECT * FROM notifications.get_instant_order_tracking($1, $2)"; // (_creatorname, _idempotencyid)
-    private const string _insertSmsNotificationSql = "call notifications.insertsmsnotification($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"; // (__orderid, _alternateid, _recipientorgno, _recipientnin, _mobilenumber, _customizedbody, _result, _smscount, _resulttime, _expirytime)
+    private const string _insertorderChainSql = "call notifications.insertorderchain($1, $2, $3, $4, $5)"; // (_orderid, _idempotencyid, _creatorname, _created, _orderchain)
+    private const string _insertSmsNotificationSql = "call notifications.insertsmsnotification($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"; // (_orderid, _alternateid, _recipientorgno, _recipientnin, _mobilenumber, _customizedbody, _result, _smscount, _resulttime, _expirytime)
+    private const string _getInstantOrderTrackingInformationSql = "SELECT * FROM notifications.get_instant_order_tracking($1, $2)"; // (_creatorname, _idempotencyid)
 
     /// <summary>
     /// Initializes a new instance of the <see cref="InstantOrderRepository"/> class.
@@ -37,9 +37,7 @@ public class InstantOrderRepository : IInstantOrderRepository
         string ordersChainIdColumnName = "orders_chain_id";
         string senderReferenceColumnName = "senders_reference";
 
-        cancellationToken.ThrowIfCancellationRequested();
-
-        await using NpgsqlCommand command = _dataSource.CreateCommand(_getInstantOrderTrackingSql);
+        await using NpgsqlCommand command = _dataSource.CreateCommand(_getInstantOrderTrackingInformationSql);
         command.Parameters.AddWithValue(NpgsqlDbType.Text, creatorName);
         command.Parameters.AddWithValue(NpgsqlDbType.Text, idempotencyId);
 
@@ -81,27 +79,20 @@ public class InstantOrderRepository : IInstantOrderRepository
     /// <inheritdoc/>
     public async Task<InstantNotificationOrderTracking?> PersistInstantSmsNotificationAsync(InstantNotificationOrder instantNotificationOrder, NotificationOrder notificationOrder, SmsNotification smsNotification, DateTime smsExpiryTime, int smsMessageCount, CancellationToken cancellationToken = default)
     {
-        cancellationToken.ThrowIfCancellationRequested();
-
         await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
         await using var transaction = await connection.BeginTransactionAsync(cancellationToken);
 
         try
         {
-            cancellationToken.ThrowIfCancellationRequested();
             await InsertInstantNotificationOrderAsync(instantNotificationOrder, connection, transaction, cancellationToken);
 
-            cancellationToken.ThrowIfCancellationRequested();
             long mainOrderId = await InsertNotificationOrderAsync(notificationOrder, connection, transaction, OrderProcessingStatus.Processed, cancellationToken);
 
-            cancellationToken.ThrowIfCancellationRequested();
             var smsTemplate = notificationOrder.Templates.Find(e => e.Type == NotificationTemplateType.Sms) as SmsTemplate;
             await InsertSmsTextAsync(mainOrderId, smsTemplate!, connection, transaction, cancellationToken);
 
-            cancellationToken.ThrowIfCancellationRequested();
             await InsertSmsNotificationAsync(smsNotification, smsExpiryTime, smsMessageCount, connection, transaction, cancellationToken);
 
-            cancellationToken.ThrowIfCancellationRequested();
             await transaction.CommitAsync(cancellationToken);
         }
         catch (OperationCanceledException)
@@ -127,7 +118,7 @@ public class InstantOrderRepository : IInstantOrderRepository
     }
 
     /// <summary>
-    /// Persists the SMS template details for a notification order.
+    /// Persists the SMS texts for a notification order.
     /// </summary>
     /// <param name="notificationOrderId">
     /// The unique identifier of the notification order in the database.
@@ -162,7 +153,7 @@ public class InstantOrderRepository : IInstantOrderRepository
     /// Persists an instant notification order to the database.
     /// </summary>
     /// <param name="instantNotificationOrder">
-    /// The high-priority notification order to be persisted, containing recipient and delivery details.
+    /// The high-priority notification order to be persisted.
     /// </param>
     /// <param name="connection">
     /// The active PostgreSQL database connection.
@@ -176,7 +167,7 @@ public class InstantOrderRepository : IInstantOrderRepository
     /// <returns>A task representing the asynchronous operation.</returns>
     private static async Task InsertInstantNotificationOrderAsync(InstantNotificationOrder instantNotificationOrder, NpgsqlConnection connection, NpgsqlTransaction transaction, CancellationToken cancellationToken = default)
     {
-        await using NpgsqlCommand pgcom = new(_insertorderchainSql, connection, transaction);
+        await using NpgsqlCommand pgcom = new(_insertorderChainSql, connection, transaction);
 
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, instantNotificationOrder.OrderChainId);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, instantNotificationOrder.IdempotencyId);
@@ -217,10 +208,10 @@ public class InstantOrderRepository : IInstantOrderRepository
 
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, notification.OrderId);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, notification.Id);
-        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, DBNull.Value);
-        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, DBNull.Value);
+        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, notification.Recipient.OrganizationNumber ?? (object)DBNull.Value);
+        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, notification.Recipient.NationalIdentityNumber ?? (object)DBNull.Value);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, notification.Recipient.MobileNumber);
-        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, DBNull.Value);
+        pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, notification.Recipient.CustomizedBody ?? (object)DBNull.Value);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, notification.SendResult.Result.ToString());
         pgcom.Parameters.AddWithValue(NpgsqlDbType.Integer, smsMessageCount);
         pgcom.Parameters.AddWithValue(NpgsqlDbType.TimestampTz, notification.SendResult.ResultTime);
@@ -230,10 +221,10 @@ public class InstantOrderRepository : IInstantOrderRepository
     }
 
     /// <summary>
-    /// Persists a notification order to the database and returns the generated order identifier.
+    /// Persists a notification order to the database and returns the its identifier.
     /// </summary>
     /// <param name="order">
-    /// The <see cref="NotificationOrder"/> containing the details of the notification order to be persisted.
+    /// The <see cref="NotificationOrder"/> to be persisted.
     /// </param>
     /// <param name="connection">
     /// The active <see cref="NpgsqlConnection"/> to the PostgreSQL database.
