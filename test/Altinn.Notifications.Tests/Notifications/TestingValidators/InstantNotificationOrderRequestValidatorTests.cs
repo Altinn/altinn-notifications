96633d67d939dbaba5e50277ce66d3c71e264203
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-16 13:12:04 +0200

    Write simple tests to validate validation logic

---
+++ test/Altinn.Notifications.Tests/Notifications/TestingValidators/InstantNotificationOrderRequestValidatorTests.cs
@@ -0,0 +1,72 @@
+﻿using Altinn.Notifications.Models.Orders;
+using Altinn.Notifications.Models.Recipient;
+using Altinn.Notifications.Models.Sms;
+using Altinn.Notifications.Validators.Orders;
+
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.TestingValidators;
+
+public class InstantNotificationOrderRequestValidatorTests
+{
+    private readonly InstantNotificationOrderRequestValidator _validator;
+
+    public InstantNotificationOrderRequestValidatorTests()
+    {
+        _validator = new InstantNotificationOrderRequestValidator();
+    }
+
+    [Fact]
+    public void Validate_WithValidRecipient_ReturnsValid()
+    {
+        var request = new InstantNotificationOrderRequestExt
+        {
+            IdempotencyId = "EB08BC4E-A35A-4B37-841A-9EC5D41E3E14",
+            SendersReference = "AE16178F-8C72-4C6B-AE7E-E18D60B7BF94",
+            InstantNotificationRecipient = new InstantNotificationRecipientExt
+            {
+                ShortMessageDeliveryDetails = new ShortMessageDeliveryDetailsExt
+                {
+                    TimeToLiveInSeconds = 3600,
+                    PhoneNumber = "+4799999999",
+                    ShortMessageContent = new ShortMessageContentExt
+                    {
+                        Sender = "Altinn",
+                        Body = "Test message"
+                    }
+                }
+            }
+        };
+
+        var validationResult = _validator.Validate(request);
+
+        Assert.True(validationResult.IsValid);
+    }
+
+    [Fact]
+    public void Validate_WithInvalidRecipient_ReturnsInvalid()
+    {
+        var request = new InstantNotificationOrderRequestExt
+        {
+            IdempotencyId = "39A3D898-37D6-4507-A7AE-27FECD63884C",
+            SendersReference = "6C176F56-F69E-445B-9631-BAA68F0D5AB3",
+            InstantNotificationRecipient = new InstantNotificationRecipientExt
+            {
+                ShortMessageDeliveryDetails = new ShortMessageDeliveryDetailsExt
+                {
+                    TimeToLiveInSeconds = 3600,
+                    PhoneNumber = "invalid-number",
+                    ShortMessageContent = new ShortMessageContentExt
+                    {
+                        Sender = "Altinn",
+                        Body = "Test message"
+                    }
+                }
+            }
+        };
+
+        var validationResult = _validator.Validate(request);
+
+        Assert.False(validationResult.IsValid);
+    }
+}
