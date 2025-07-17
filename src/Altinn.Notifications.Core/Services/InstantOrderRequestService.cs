f16f1a670ab9c5878f5bc0eefe3d593c6fe6b886
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-17 14:16:25 +0200

    Move the logic that is used to send the SMS to the Service

--- src/Altinn.Notifications.Core/Services/InstantOrderRequestService.cs
+++ src/Altinn.Notifications.Core/Services/InstantOrderRequestService.cs
@@ -2,11 +2,14 @@
 
 using Altinn.Notifications.Core.Configuration;
 using Altinn.Notifications.Core.Enums;
+using Altinn.Notifications.Core.Integrations;
+using Altinn.Notifications.Core.Models;
 using Altinn.Notifications.Core.Models.Address;
 using Altinn.Notifications.Core.Models.Notification;
 using Altinn.Notifications.Core.Models.NotificationTemplate;
 using Altinn.Notifications.Core.Models.Orders;
 using Altinn.Notifications.Core.Models.Recipients;
+using Altinn.Notifications.Core.Models.ShortMessageService;
 using Altinn.Notifications.Core.Persistence;
 using Altinn.Notifications.Core.Services.Interfaces;
 
@@ -15,14 +18,16 @@ using Microsoft.Extensions.Options;
 namespace Altinn.Notifications.Core.Services;
 
 /// <summary>
-/// Provides the concrete implementation of <see cref="IInstantOrderRequestService"/> for registering and tracking instant notification orders.
+/// Implements the <see cref="IInstantOrderRequestService"/> interface
+/// to handle the registration, persistence, and tracking of instant notification orders for SMS delivery.
 /// </summary>
-internal class InstantOrderRequestService : IInstantOrderRequestService
+public class InstantOrderRequestService : IInstantOrderRequestService
 {
     private readonly string _defaultSmsSender;
     private readonly IGuidService _guidService;
     private readonly IDateTimeService _dateTimeService;
     private readonly IOrderRepository _orderRepository;
+    private readonly IShortMessageServiceClient _shortMessageServiceClient;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="InstantOrderRequestService"/> class.
@@ -31,11 +36,13 @@ internal class InstantOrderRequestService : IInstantOrderRequestService
         IGuidService guidService,
         IDateTimeService dateTimeService,
         IOrderRepository instantOrderRepository,
-        IOptions<NotificationConfig> configurationOptions)
+        IOptions<NotificationConfig> configurationOptions,
+        IShortMessageServiceClient shortMessageServiceClient)
     {
         _guidService = guidService;
         _dateTimeService = dateTimeService;
         _orderRepository = instantOrderRepository;
+        _shortMessageServiceClient = shortMessageServiceClient;
         _defaultSmsSender = configurationOptions.Value.DefaultSmsSenderNumber;
     }
 
@@ -48,47 +55,41 @@ internal class InstantOrderRequestService : IInstantOrderRequestService
     /// <inheritdoc/>
     public async Task<InstantNotificationOrderTracking?> PersistInstantSmsNotificationAsync(InstantNotificationOrder instantNotificationOrder, CancellationToken cancellationToken = default)
     {
-        cancellationToken.ThrowIfCancellationRequested();
-
         var deliveryDetails = instantNotificationOrder.InstantNotificationRecipient.ShortMessageDeliveryDetails;
         var messageContent = instantNotificationOrder.InstantNotificationRecipient.ShortMessageDeliveryDetails.ShortMessageContent;
 
-        int messagesCount = CalculateNumberOfMessages(messageContent.Message);
-        var expirationDateTime = instantNotificationOrder.Created.AddSeconds(deliveryDetails.TimeToLiveInSeconds);
-        var senderIdentifier = string.IsNullOrWhiteSpace(messageContent.Sender) ? _defaultSmsSender : messageContent.Sender;
+        var messagesCount = CalculateNumberOfMessages(messageContent.Message);
 
-        var notificationOrder = new NotificationOrder
-        {
-            ResourceId = null,
-            IgnoreReservation = null,
-            ConditionEndpoint = null,
-            Type = instantNotificationOrder.Type,
-            Id = instantNotificationOrder.OrderId,
-            Creator = instantNotificationOrder.Creator,
-            Created = instantNotificationOrder.Created,
-            SendingTimePolicy = SendingTimePolicy.Anytime,
-            NotificationChannel = NotificationChannel.Sms,
-            RequestedSendTime = instantNotificationOrder.Created,
-            SendersReference = instantNotificationOrder.SendersReference,
-            Recipients = [new([new SmsAddressPoint(deliveryDetails.PhoneNumber)])],
-            Templates = [new SmsTemplate(senderIdentifier, messageContent.Message)]
-        };
+        var smsNotification = CreateSmsNotification(instantNotificationOrder, deliveryDetails);
 
-        var smsNotification = new SmsNotification()
-        {
-            Id = _guidService.NewGuid(),
-            Recipient = new SmsRecipient()
-            {
-                MobileNumber = deliveryDetails.PhoneNumber
-            },
-            OrderId = instantNotificationOrder.OrderId,
-            RequestedSendTime = instantNotificationOrder.Created,
-            SendResult = new(SmsNotificationResultType.Sending, _dateTimeService.UtcNow())
-        };
+        var notificationOrder = CreateNotificationOrder(instantNotificationOrder, deliveryDetails, messageContent);
+
+        var expirationDateTime = notificationOrder.RequestedSendTime.AddSeconds(deliveryDetails.TimeToLiveInSeconds);
 
         cancellationToken.ThrowIfCancellationRequested();
 
-        return await _orderRepository.Create(instantNotificationOrder, notificationOrder, smsNotification, expirationDateTime, messagesCount, cancellationToken);
+        // Create the tracking information for the order.
+        var trackingInformation = await _orderRepository.Create(instantNotificationOrder, notificationOrder, smsNotification, expirationDateTime, messagesCount, cancellationToken);
+        if (trackingInformation != null)
+        {
+            _ = Task.Run(
+                async () =>
+                {
+                    var shortMessage = new ShortMessage
+                    {
+                        Message = messageContent.Message,
+                        NotificationId = smsNotification.Id,
+                        TimeToLive = deliveryDetails.TimeToLiveInSeconds,
+                        Recipient = smsNotification.Recipient.MobileNumber,
+                        Sender = string.IsNullOrWhiteSpace(messageContent.Sender) ? _defaultSmsSender : messageContent.Sender
+                    };
+
+                    await _shortMessageServiceClient.SendAsync(shortMessage);
+                },
+                CancellationToken.None);
+        }
+
+        return trackingInformation;
     }
 
     /// <summary>
@@ -123,5 +124,69 @@ internal class InstantOrderRequestService : IInstantOrderRequestService
         }
 
         return numberOfMessages;
+    }
+
+    /// <summary>
+    /// Creates a <see cref="SmsNotification"/> instance for an instant SMS notification.
+    /// </summary>
+    /// <param name="instantNotificationOrder">
+    /// The <see cref="InstantNotificationOrder"/> containing core order metadata such as type, ID, creator, and timestamps.
+    /// </param>
+    /// <param name="deliveryDetails">
+    /// The <see cref="ShortMessageDeliveryDetails"/> containing the recipient's phone number.
+    /// </param>
+    /// <returns>
+    /// A <see cref="SmsNotification"/> object populated for SMS delivery.
+    /// </returns>
+    private SmsNotification CreateSmsNotification(InstantNotificationOrder instantNotificationOrder, ShortMessageDeliveryDetails deliveryDetails)
+    {
+        return new SmsNotification
+        {
+            Id = _guidService.NewGuid(),
+            Recipient = new SmsRecipient
+            {
+                MobileNumber = deliveryDetails.PhoneNumber
+            },
+            OrderId = instantNotificationOrder.OrderId,
+            RequestedSendTime = instantNotificationOrder.Created,
+            SendResult = new(SmsNotificationResultType.Sending, _dateTimeService.UtcNow())
+        };
+    }
+
+    /// <summary>
+    /// Creates a <see cref="NotificationOrder"/> instance for an instant SMS notification.
+    /// </summary>
+    /// <param name="instantNotificationOrder">
+    /// The <see cref="InstantNotificationOrder"/> containing core order metadata such as type, ID, creator, and timestamps.
+    /// </param>
+    /// <param name="deliveryDetails">
+    /// The <see cref="ShortMessageDeliveryDetails"/> containing recipient phone number and time-to-live.
+    /// </param>
+    /// <param name="messageContent">
+    /// The <see cref="ShortMessageContent"/> containing the SMS message body and sender information.
+    /// </param>
+    /// <returns>
+    /// A <see cref="NotificationOrder"/> object populated for SMS delivery.
+    /// </returns>
+    private NotificationOrder CreateNotificationOrder(InstantNotificationOrder instantNotificationOrder, ShortMessageDeliveryDetails deliveryDetails, ShortMessageContent messageContent)
+    {
+        var senderIdentifier = string.IsNullOrWhiteSpace(messageContent.Sender) ? _defaultSmsSender : messageContent.Sender;
+
+        return new NotificationOrder
+        {
+            ResourceId = null,
+            IgnoreReservation = null,
+            ConditionEndpoint = null,
+            Type = instantNotificationOrder.Type,
+            Id = instantNotificationOrder.OrderId,
+            Creator = instantNotificationOrder.Creator,
+            Created = instantNotificationOrder.Created,
+            SendingTimePolicy = SendingTimePolicy.Anytime,
+            NotificationChannel = NotificationChannel.Sms,
+            RequestedSendTime = instantNotificationOrder.Created,
+            SendersReference = instantNotificationOrder.SendersReference,
+            Recipients = [new([new SmsAddressPoint(deliveryDetails.PhoneNumber)])],
+            Templates = [new SmsTemplate(senderIdentifier, messageContent.Message)]
+        };
     }
 }
