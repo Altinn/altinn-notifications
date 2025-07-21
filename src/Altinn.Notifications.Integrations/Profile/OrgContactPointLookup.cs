631ee61b7aa1f88da644e25c4330506c8ab60a3a
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-07-21 16:55:54 +0200

    Move request and response model to Profile namespace

---
+++ src/Altinn.Notifications.Integrations/Profile/OrgContactPointLookup.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Integrations.Profile
+{
+    /// <summary>
+    /// A class describing the query model for contact points for organizations
+    /// </summary>
+    public class OrgContactPointLookup
+    {
+        /// <summary>
+        /// Gets or sets the list of organization numbers to lookup contact points for
+        /// </summary>
+        [JsonPropertyName("organizationNumbers")]
+        public List<string> OrganizationNumbers { get; set; } = [];
+    }
+}
