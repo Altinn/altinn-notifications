627cb7ac9f6532adc59a125c563556ffa68cd000
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-21 11:21:24 +0200

    Initialize the URI outside the constructor scope

--- src/Altinn.Notifications.Integrations/ShortMessageService/ShortMessageServiceClient.cs
+++ src/Altinn.Notifications.Integrations/ShortMessageService/ShortMessageServiceClient.cs
@@ -15,9 +15,9 @@ namespace Altinn.Notifications.Integrations.ShortMessageService;
 /// </summary>
 public class ShortMessageServiceClient : IShortMessageServiceClient
 {
-    private readonly Uri _sendEndpoint;
     private readonly HttpClient _httpClient;
     private readonly ILogger<ShortMessageServiceClient> _logger;
+    private readonly Uri _sendEndpoint = new("instantmessage/send", UriKind.Relative);
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ShortMessageServiceClient"/> class.
@@ -29,7 +29,6 @@ public class ShortMessageServiceClient : IShortMessageServiceClient
     {
         _logger = logger;
         _httpClient = httpClient;
-        _sendEndpoint = new Uri("instantmessage/send", UriKind.Relative);
         _httpClient.BaseAddress = new Uri(platformSettings.Value.ApiShortMessageServiceEndpoint);
     }
 
