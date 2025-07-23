ab882205c0c74e4cc13f5a13cab7021637d25051
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-07-23 12:53:00 +0200

    Add support for nin recipient for email

--- test/k6/src/tests/orders-v2.js
+++ test/k6/src/tests/orders-v2.js
@@ -10,6 +10,7 @@
     -e encodedJwk={the encoded JSON web key used to sign the maskinporten token request} \
     -e env={environment: at22, at23, at24, tt02, prod} \
     -e emailRecipient={an email address to add as a notification recipient} \
+    -e ninRecipient={a national identity number of a person to include as a notification recipient} \
     -e smsRecipient={a mobile number to include as a notification recipient} \
     -e subscriptionKey={the subscription key with access to the automated tests product} \
 
@@ -78,6 +79,10 @@ setEmptyThresholds(labels, options);
 export function setup() {
     const emailRecipient = getEmailRecipient();
     const smsRecipient = getSmsRecipient();
+
+    // used with notification email orders if applicable
+    const ninRecipient = __ENV.ninRecipient ? __ENV.ninRecipient.toLowerCase() : null;
+
     const token = setupToken.getAltinnTokenForOrg(scopes);
     const idempotencyIdEmail = uuidv4();
     const idempotencyIdSms = uuidv4();
@@ -85,6 +90,18 @@ export function setup() {
 
     if (emailRecipient) {
         emailOrderRequestJson.recipient.recipientEmail.emailAddress = emailRecipient;
+    }
+    else {
+        // unset recipientEmail object when no email recipient is provided
+        delete emailOrderRequestJson.recipient["recipientEmail"];
+    }
+
+    if (ninRecipient) {
+        emailOrderRequestJson.recipient.recipientPerson.nationalIdentityNumber = ninRecipient;
+    }
+    else {
+        // unset recipientPerson object when no national identity number is provided
+        delete emailOrderRequestJson.recipient["recipientPerson"];
     }
 
     if (smsRecipient) {
@@ -150,6 +167,8 @@ function postSmsNotificationOrderRequest(data) {
     const success = check(response, {
         "POST SMS notification order request. Status is 201 Created": (r) => r.status === 201
     });
+
+    console.log(response);
 
     stopIterationOnFail("POST SMS notification order request failed", success);
 
