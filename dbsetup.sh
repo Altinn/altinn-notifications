6e2f4cb926fa93e1d5409fc21fd061cff953eff8
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-07-28 09:53:23 +0200

    Set max idle time

--- dbsetup.sh
+++ dbsetup.sh
@@ -3,7 +3,10 @@ export PGPASSWORD=Password
 
 # alter max connections
 psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb  \
--c "ALTER SYSTEM SET max_connections TO '250';"
+-c "ALTER SYSTEM SET max_connections TO '200';"
+
+# Terminates any session idle for more than 10 seconds
+-c "ALTER SYSTEM SET idle_session_timeout TO '5s';"
 
 # set up platform_notifications role
 psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb \
@@ -11,3 +14,4 @@ psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb \
     BEGIN CREATE ROLE platform_notifications WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
     END \$\$;"
+    
