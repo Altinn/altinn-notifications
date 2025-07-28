59a636b82865fe644858d24a214627109d80839a
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-07-28 10:32:15 +0200

    Add pooling=false for admin

--- dbsetup.sh
+++ dbsetup.sh
@@ -5,13 +5,9 @@ export PGPASSWORD=Password
 psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb  \
 -c "ALTER SYSTEM SET max_connections TO '200';"
 
-# Terminates any session idle for more than 10 seconds
--c "ALTER SYSTEM SET idle_session_timeout TO '5s';"
-
 # set up platform_notifications role
 psql -h localhost -p 5432 -U platform_notifications_admin -d notificationsdb \
 -c "DO \$\$
     BEGIN CREATE ROLE platform_notifications WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
     END \$\$;"
-    
