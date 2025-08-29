8d385666a253ac077d4bba61b381c762f7afcfa8
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-08-29 09:45:41 +0200

    Implement a custom exception type to use when a status update fails.

---
+++ src/Altinn.Notifications.Core/Enums/SendStatusIdentifierType.cs
@@ -0,0 +1,22 @@
+﻿namespace Altinn.Notifications.Core.Enums;
+
+/// <summary>
+/// Identifies which key was used to perform the status update.
+/// </summary>
+public enum SendStatusIdentifierType
+{
+    /// <summary>
+    /// The notification identifier.
+    /// </summary>
+    NotificationId,
+
+    /// <summary>
+    /// Azure Communication Services operation identifier (Email).
+    /// </summary>
+    OperationId,
+
+    /// <summary>
+    /// LinkMobility gateway reference (SMS).
+    /// </summary>
+    GatewayReference
+}
