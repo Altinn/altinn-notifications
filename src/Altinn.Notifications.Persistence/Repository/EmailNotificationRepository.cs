e6724a838728b6bd0f015bf71e23a3c724474f7b
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-09-15 10:51:07 +0200

    Simplify the condition checking

--- src/Altinn.Notifications.Persistence/Repository/EmailNotificationRepository.cs
+++ src/Altinn.Notifications.Persistence/Repository/EmailNotificationRepository.cs
@@ -111,8 +111,8 @@ public class EmailNotificationRepository : NotificationRepositoryBase, IEmailNot
         {
             await using NpgsqlCommand pgcom = new(_updateEmailStatus, connection, transaction);
             pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, status.ToString());
-            pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, string.IsNullOrWhiteSpace(operationId) ? (object)DBNull.Value : operationId);
-            pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, notificationId ?? (object)DBNull.Value);
+            pgcom.Parameters.AddWithValue(NpgsqlDbType.Text, string.IsNullOrWhiteSpace(operationId) ? DBNull.Value : operationId);
+            pgcom.Parameters.AddWithValue(NpgsqlDbType.Uuid, (notificationId == null || notificationId == Guid.Empty) ? DBNull.Value : notificationId);
 
             var alternateId = await pgcom.ExecuteScalarAsync();
             if (alternateId is null)
