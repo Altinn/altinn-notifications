825cfc778b86372eb2e3132197d89ce3b0982d4e
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-09-16 09:37:20 +0200

    Add test class for getstatusfeedrequest

---
+++ test/Altinn.Notifications.Tests/Notifications/TestingValidators/GetStatusFeedRequestValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Altinn.Notifications.Core.Models.Status;
+using Altinn.Notifications.Validators;
+
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Altinn.Notifications.Tests.Notifications.TestingValidators
+{
+    public class GetStatusFeedRequestValidatorTests
+    {
+        private readonly GetStatusFeedRequestValidator _sut = new();
+
+        [Fact]
+        public void Should_Have_Validation_Error_For_Seq_When_Negative()
+        {
+            // arrange
+            var request = new GetStatusFeedRequest
+            {
+                Seq = -1,
+            };
+
+            // act
+            var actual = _sut.TestValidate(request);
+
+            // assert
+            Assert.False(actual.IsValid);
+            Assert.Contains(actual.Errors, e => e.PropertyName == "Seq");
+            Assert.Contains(actual.Errors, e => e.ErrorMessage == "Sequence number cannot be less than 0.");
+        }
+    }
+}
