bb6f235a07d2e12ed36afc8f1d934842fe5d8f84
martivj <martin.vagseter.jakobsen@digdir.no>
2025-10-16 10:57:39 +0200

    Fix Byte Order Mark diff with checkout

--- src/Altinn.Notifications.Integrations/Kafka/Consumers/NotificationStatusRetryConsumerBase.cs
+++ src/Altinn.Notifications.Integrations/Kafka/Consumers/NotificationStatusRetryConsumerBase.cs
@@ -1,4 +1,4 @@
-using System.Text.Json;
+﻿using System.Text.Json;
 
 using Altinn.Notifications.Core;
 using Altinn.Notifications.Core.Enums;
--- src/Altinn.Notifications.Persistence/Repository/DeadDeliveryReportRepository.cs
+++ src/Altinn.Notifications.Persistence/Repository/DeadDeliveryReportRepository.cs
@@ -1,4 +1,4 @@
-using System.Data;
+﻿using System.Data;
 
 using Altinn.Notifications.Core.Models;
 using Altinn.Notifications.Core.Persistence;
