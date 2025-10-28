9556400f922078b63b05e3c9af2497c269e2742b
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-10-28 12:59:43 +0100

    Drop older index

--- src/Altinn.Notifications.Persistence/Migration/v0.61/01-alter-table.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.61/01-alter-table.sql
@@ -6,3 +6,6 @@ CREATE INDEX IF NOT EXISTS notifications_emailnotifications_result_new
   ON notifications.emailnotifications (_id)
   INCLUDE (_orderid)
   WHERE result = 'New';
+
+  -- Drop older, less specific index if it exists
+  DROP INDEX IF EXISTS notifications.notifications_emailnotifications_result;
