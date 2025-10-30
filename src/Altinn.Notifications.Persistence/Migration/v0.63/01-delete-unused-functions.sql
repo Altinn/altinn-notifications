410eff4658b8f80b1f1e23f552ce66fe7e222bd5
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-10-30 11:08:04 +0100

    Revert "Remove unused procedure"
    This reverts commit 9e3e0507ea85d249957c00105ac1757c2e01b806.

--- src/Altinn.Notifications.Persistence/Migration/v0.63/01-delete-unused-functions.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.63/01-delete-unused-functions.sql
@@ -1,5 +1,3 @@
 DROP FUNCTION IF EXISTS notifications.getsms_statusnew_updatestatus(_sendingtimepolicy integer);
 
 DROP FUNCTION IF EXISTS notifications.getsms_statusnew_updatestatus();
-
-DROP PROCEDURE IF EXISTS notifications.updateemailstatus(IN _alternateid uuid, IN _result text, IN _operationid text);
