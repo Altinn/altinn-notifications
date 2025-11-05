d3dcd80b35782ee6a8cad45146e1ec440d430e01
martivj <martin.vagseter.jakobsen@digdir.no>
2025-11-05 12:34:49 +0100

    Prep migrations for merge

--- src/Altinn.Notifications.Persistence/Migration/v0.64/01-add-dead-delivery-report-columns.sql
+++
@@ -1,4 +0,0 @@
--- Add reason and message columns to deaddeliveryreports table
-ALTER TABLE notifications.deaddeliveryreports
-ADD COLUMN IF NOT EXISTS reason TEXT,
-ADD COLUMN IF NOT EXISTS message TEXT;
