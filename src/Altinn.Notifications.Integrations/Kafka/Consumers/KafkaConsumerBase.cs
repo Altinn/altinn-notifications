09e14304d130c1d289371bd80f2984e39242aaee
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-11-14 16:58:37 +0100

    Fix a typo

--- src/Altinn.Notifications.Integrations/Kafka/Consumers/KafkaConsumerBase.cs
+++ src/Altinn.Notifications.Integrations/Kafka/Consumers/KafkaConsumerBase.cs
@@ -44,8 +44,8 @@ public abstract class KafkaConsumerBase<T> : BackgroundService
             EnableAutoCommit = false,
             EnableAutoOffsetStore = false,
             AutoOffsetReset = AutoOffsetReset.Latest,
-            GroupId = $"{settings.Consumer.GroupId}-{topicName.Replace(".", "-")}"
-            PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+            PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky,
+            GroupId = $"{settings.Value.Consumer.GroupId}-{topicName.Replace(".", "-")}"
         };
 
         _consumer = new ConsumerBuilder<string, string>(consumerConfig)
