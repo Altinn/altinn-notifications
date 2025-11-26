7884c0c4f395391ba7c3e4a5e30127e29836f25d
martivj <martin.vagseter.jakobsen@digdir.no>
2025-11-26 15:41:05 +0100

    Revert test overwrites

--- test/Altinn.Notifications.IntegrationTests/Notifications/InstantOrdersController/InstantOrdersControllerTests.cs
+++ test/Altinn.Notifications.IntegrationTests/Notifications/InstantOrdersController/InstantOrdersControllerTests.cs
@@ -463,7 +463,6 @@ public class InstantOrdersControllerTests : IClassFixture<IntegrationTestWebAppl
 
         // Assert
         Assert.Equal(500, (int)response.StatusCode);
-        Assert.Equal(500, (int)response.StatusCode);
 
         Assert.NotNull(problem);
         Assert.Equal(500, problem.Status);
