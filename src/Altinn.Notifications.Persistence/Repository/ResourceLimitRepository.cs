1ab947c7b056a87ad3be5d29be77bb005c87e981
Espen Gulbrandsen <espengulbrandsen@gmail.com>
2025-11-26 12:11:04 +0100

    Ensure that limit reached for email sending is properly registered even when the table is empty (#1180)
    * Add insert on no rows affected with tests
    
    * Add thread-safe function
    
    * Add advisory lock and function-call to avoid concurrency issues
    
    * Fix comment on function
    
    * Add exlusion attribute to repository class
    
    * Update src/Altinn.Notifications.Persistence/Migration/v0.71/01-setup-grants.sql
    
    Co-authored-by: Terje Holene <terje.holene@digdir.no>
    
    ---------
    
    Co-authored-by: Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
    Co-authored-by: Terje Holene <terje.holene@digdir.no>

--- src/Altinn.Notifications.Persistence/Repository/ResourceLimitRepository.cs
+++ src/Altinn.Notifications.Persistence/Repository/ResourceLimitRepository.cs
@@ -1,7 +1,8 @@
-﻿using Altinn.Notifications.Core.Persistence;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Altinn.Notifications.Core.Persistence;
 
 using Npgsql;
-
 using NpgsqlTypes;
 
 namespace Altinn.Notifications.Persistence.Repository
@@ -9,29 +10,22 @@ namespace Altinn.Notifications.Persistence.Repository
     /// <summary>
     /// Implementation of the <see cref="IResourceLimitRepository"/> interface
     /// </summary>
-    public class ResourceLimitRepository : IResourceLimitRepository
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="ResourceLimitRepository"/> class.
+    /// </remarks>
+    [ExcludeFromCodeCoverage]
+    public class ResourceLimitRepository(NpgsqlDataSource dataSource) : IResourceLimitRepository
     {
-        private readonly NpgsqlDataSource _dataSource;
-        private const string _setEmailTimeout = @"UPDATE notifications.resourcelimitlog
-                                                SET emaillimittimeout = $1
-                                                WHERE id = (SELECT MAX(id) FROM notifications.resourcelimitlog);";
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ResourceLimitRepository"/> class.
-        /// </summary>
-        public ResourceLimitRepository(NpgsqlDataSource dataSource)
-        {
-            _dataSource = dataSource;
-        }
+        private readonly NpgsqlDataSource _dataSource = dataSource;
+        private const string _setEmailTimeout = @"SELECT notifications.set_email_timeout(@timestamp);";
 
         /// <inheritdoc/>
         public async Task<bool> SetEmailTimeout(DateTime timeout)
         {
             await using NpgsqlCommand pgcom = _dataSource.CreateCommand(_setEmailTimeout);
-            pgcom.Parameters.AddWithValue(NpgsqlDbType.TimestampTz, timeout);
-            await pgcom.ExecuteNonQueryAsync();
-
-            return true;
+            pgcom.Parameters.AddWithValue("timestamp", NpgsqlDbType.TimestampTz, timeout);
+            var result = await pgcom.ExecuteScalarAsync();
+            return result is bool success && success;
         }
     }
 }
