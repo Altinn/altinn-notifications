6f0cec8b151cd3ce600797dc31efd1f7cb2ea839
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-11-27 08:10:04 +0100

    Rename a parameter

--- src/Altinn.Notifications.Core/Integrations/IKafkaProducer.cs
+++ src/Altinn.Notifications.Core/Integrations/IKafkaProducer.cs
@@ -10,23 +10,23 @@ public interface IKafkaProducer
     /// <summary>
     /// Publishes a single message to the specified Kafka topic.
     /// </summary>
-    /// <param name="topic">The name of the Kafka topic to publish the message to.</param>
+    /// <param name="topicName">The name of the Kafka topic to publish the message to.</param>
     /// <param name="message">The content of the message to be published.</param>
     /// <returns>
     /// A <see cref="Task{TResult}"/> representing the asynchronous operation.
     /// Returns <c>true</c> if the message was successfully published; otherwise, <c>false</c>.
     /// </returns>
-    Task<bool> ProduceAsync(string topic, string message);
+    Task<bool> ProduceAsync(string topicName, string message);
 
     /// <summary>
     /// Publishes a batch of messages to the specified Kafka topic.
     /// </summary>
-    /// <param name="topic">The name of the Kafka topic to publish the messages to.</param>
+    /// <param name="topicName">The name of the Kafka topic to publish the messages to.</param>
     /// <param name="messages">A collection of message payloads to be published.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
     /// <returns>
     /// A <see cref="Task{TResult}"/> representing the asynchronous operation.
     /// Returns a collection of messages that were not successfully published.
     /// </returns>
-    Task<IEnumerable<string>> ProduceAsync(string topic, IImmutableList<string> messages, CancellationToken cancellationToken = default);
+    Task<IEnumerable<string>> ProduceAsync(string topicName, IImmutableList<string> messages, CancellationToken cancellationToken = default);
 }
