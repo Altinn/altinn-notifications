eddd8440304362f83a3d25e02f9570e49578e0d4
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-12-01 07:59:39 +0100

    Remove unused directives

--- src/Altinn.Notifications.Core/Services/SmsNotificationService.cs
+++ src/Altinn.Notifications.Core/Services/SmsNotificationService.cs
@@ -1,4 +1,6 @@
-﻿using Altinn.Notifications.Core.Configuration;
+﻿using System.Text.Json;
+
+using Altinn.Notifications.Core.Configuration;
 using Altinn.Notifications.Core.Enums;
 using Altinn.Notifications.Core.Integrations;
 using Altinn.Notifications.Core.Models;
@@ -8,8 +10,6 @@ using Altinn.Notifications.Core.Models.Recipients;
 using Altinn.Notifications.Core.Persistence;
 using Altinn.Notifications.Core.Services.Interfaces;
 using Microsoft.Extensions.Options;
-using System.Collections.Immutable;
-using System.Text.Json;
 
 namespace Altinn.Notifications.Core.Services;
 
