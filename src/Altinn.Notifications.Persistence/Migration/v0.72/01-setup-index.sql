639c19a40e65158846455f1a6c2a14cd86e193ac
martivj <martin.vagseter.jakobsen@digdir.no>
2025-12-13 12:49:52 +0100

    Add rationale comment

--- src/Altinn.Notifications.Persistence/Migration/v0.72/01-setup-index.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.72/01-setup-index.sql
@@ -1,3 +1,4 @@
+-- To support efficient querying in getorders_pastsendtime function
 CREATE INDEX IF NOT EXISTS notifications_registered_reqtime_idx
 ON notifications.orders (requestedsendtime, _id)
 WHERE processedstatus = 'Registered';
