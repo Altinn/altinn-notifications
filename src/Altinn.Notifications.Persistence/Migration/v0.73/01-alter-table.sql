3647d21a7f130d005eda968a95d4dad3ab3ba65d
martivj <martin.vagseter.jakobsen@digdir.no>
2026-01-12 09:32:26 +0100

    Fix naming conventions

--- src/Altinn.Notifications.Persistence/Migration/v0.73/01-alter-table.sql
+++ src/Altinn.Notifications.Persistence/Migration/v0.73/01-alter-table.sql
@@ -2,8 +2,8 @@
 -- The design assumes each orderid only has one entry with the final status
 
 -- Step 1: Create unique index
--- Manual command to run first: CREATE UNIQUE INDEX CONCURRENTLY statusfeed_unique_orderid_idx ON notifications.statusfeed (orderid);
-CREATE UNIQUE INDEX IF NOT EXISTS statusfeed_unique_orderid_idx ON notifications.statusfeed (orderid);
+-- Manual command to run first: CREATE UNIQUE INDEX CONCURRENTLY idx_statusfeed_orderid_unique ON notifications.statusfeed (orderid);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_statusfeed_orderid_unique ON notifications.statusfeed (orderid);
 
 -- Step 2: Add constraint using the existing index
 DO $$
@@ -14,6 +14,6 @@ BEGIN
         WHERE conname = 'statusfeed_unique_orderid'
     ) THEN
         ALTER TABLE notifications.statusfeed
-        ADD CONSTRAINT statusfeed_unique_orderid UNIQUE USING INDEX statusfeed_unique_orderid_idx;
+        ADD CONSTRAINT statusfeed_unique_orderid UNIQUE USING INDEX idx_statusfeed_orderid_unique;
     END IF;
 END $$;
