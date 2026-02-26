65cdbbc038eb24ac305328766d3c2bd3f0af1564
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2026-02-26 12:43:46 +0100

    Fix a typo

--- components/api/src/Altinn.Notifications.Core/Services/GuidService.cs
+++ components/api/src/Altinn.Notifications.Core/Services/GuidService.cs
@@ -5,7 +5,7 @@ using Altinn.Notifications.Core.Services.Interfaces;
 namespace Altinn.Notifications.Core.Services;
 
 /// <summary>
-/// Implementation of the GuidServiceS
+/// Implementation of the GuidService
 /// </summary>
 [ExcludeFromCodeCoverage]
 public class GuidService : IGuidService
