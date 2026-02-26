b65c2df7dbd7d0894f2984b9fd832b76e33806f0
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2026-02-26 13:17:00 +0100

    One more commit

--- components/api/src/Altinn.Notifications.Core/Services/GuidService.cs
+++ components/api/src/Altinn.Notifications.Core/Services/GuidService.cs
@@ -5,7 +5,7 @@ using Altinn.Notifications.Core.Services.Interfaces;
 namespace Altinn.Notifications.Core.Services;
 
 /// <summary>
-/// Service for generating
+/// Service for generating globally unique identifiers (GUIDs)
 /// </summary>
 [ExcludeFromCodeCoverage]
 public class GuidService : IGuidService
