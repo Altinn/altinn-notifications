7da96a122d5d3f22a9908187489d496c6fc7025c
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-03-20 14:43:53 +0100

    Add contract

---
+++ components/api/src/Altinn.Notifications.Core/Integrations/ISmsCommandPublisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altinn.Notifications.Core.Integrations;
+
+/// <summary>
+/// Defines the contract for publishing sms notifications from the API to the Sms Service via Azure Service Bus using Wolverine.
+/// </summary>
+public interface ISmsCommandPublisher
+{
+    /// <summary>
+    /// Publishes an asynchronous operation and returns a unique identifier for the published item.
+    /// </summary>
+    /// <remarks>This method allows for cancellation of the publish operation. If the operation is canceled,
+    /// the task will complete with a cancellation exception.</remarks>
+    /// <param name="cancellationToken">The cancellation token used to propagate notification that the operation should be canceled.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a GUID that uniquely identifies the published
+    /// item.</returns>
+    Task<Guid> PublishAsync(CancellationToken cancellationToken);
+}
---
+++ components/shared/src/Altinn.Notifications.Shared/Commands/SendSmsCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Altinn.Notifications.Shared.Commands;
+
+/// <summary>
+/// Represents a command to send an sms notification from the Notifications API to the SMS Service.
+/// </summary>
+public sealed record SendSmsCommand
+{
+}
