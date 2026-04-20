3ac009225ec7fd25812cc28d3974aadeeaf899d5
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2026-04-20 09:40:03 +0200

    Remove unused namespace

--- components/email-service/src/Altinn.Notifications.Email.Integrations/Configuration/ServiceCollectionExtensions.cs
+++ components/email-service/src/Altinn.Notifications.Email.Integrations/Configuration/ServiceCollectionExtensions.cs
@@ -8,7 +8,6 @@ using Altinn.Notifications.Email.Integrations.Publishers;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Altinn.Notifications.Email.Integrations.Configuration;
 
