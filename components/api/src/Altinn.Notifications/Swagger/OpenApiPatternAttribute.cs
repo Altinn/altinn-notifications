f7d1e0f2e50873e5938d0d0f75c04501db3a137a
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2026-04-21 08:44:36 +0200

    Clarify XML doc

--- components/api/src/Altinn.Notifications/Swagger/OpenApiPatternAttribute.cs
+++ components/api/src/Altinn.Notifications/Swagger/OpenApiPatternAttribute.cs
@@ -3,14 +3,14 @@ using System.Diagnostics.CodeAnalysis;
 namespace Altinn.Notifications.Swagger;
 
 /// <summary>
-/// A non-validating attribute that communicates a pattern constraint  
+/// A non-validating attribute that communicates the pattern constraint for a OpenAPI schema property
 /// </summary>
 [ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Property)]
 public class OpenApiPatternAttribute(string pattern) : Attribute
 {
     /// <summary>
-    /// The property's pattern attribute
+    /// The regex pattern of the property
     /// </summary>
     public string Pattern { get; } = pattern;
 }
