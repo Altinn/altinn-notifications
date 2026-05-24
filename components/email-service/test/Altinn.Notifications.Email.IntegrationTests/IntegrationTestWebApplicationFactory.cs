e5967cec24ea5f03330a49660e73fc46af65f7ce
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-05-24 22:55:14 +0200

    Remove default false value

--- components/email-service/test/Altinn.Notifications.Email.IntegrationTests/IntegrationTestWebApplicationFactory.cs
+++ components/email-service/test/Altinn.Notifications.Email.IntegrationTests/IntegrationTestWebApplicationFactory.cs
@@ -9,18 +9,12 @@ namespace Altinn.Notifications.Email.IntegrationTests;
 public class IntegrationTestWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
       where TStartup : class
 {
-    private readonly string? _originalEnableWolverine = Environment.GetEnvironmentVariable("WolverineSettings__EnableWolverine");
-
     /// <summary>
     /// Configures the web host for setting up configuration and test services.
     /// </summary>
     /// <param name="builder">The web host builder.</param>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        // Disable Wolverine/ASB globally: no ServiceBusConnectionString is configured in the
-        // test environment, so Wolverine must not attempt to connect.
-        Environment.SetEnvironmentVariable("WolverineSettings__EnableWolverine", "false");
-
         builder.ConfigureTestServices(services =>
         {
             // Remove all Kafka consumers - controller tests don't need them
@@ -33,15 +27,5 @@ public class IntegrationTestWebApplicationFactory<TStartup> : WebApplicationFact
                 services.Remove(descriptor);
             }
         });
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            Environment.SetEnvironmentVariable("WolverineSettings__EnableWolverine", _originalEnableWolverine);
-        }
-
-        base.Dispose(disposing);
     }
 }
