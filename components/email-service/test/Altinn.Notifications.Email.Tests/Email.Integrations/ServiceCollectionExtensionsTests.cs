989a53619ed1fa929e3806d1e25758b2370591c2
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-05-28 11:34:09 +0200

    Remove orphaned usings

--- components/email-service/test/Altinn.Notifications.Email.Tests/Email.Integrations/ServiceCollectionExtensionsTests.cs
+++ components/email-service/test/Altinn.Notifications.Email.Tests/Email.Integrations/ServiceCollectionExtensionsTests.cs
@@ -1,12 +1,7 @@
-using Altinn.Notifications.Email.Core;
-using Altinn.Notifications.Email.Core.Dependencies;
 using Altinn.Notifications.Email.Integrations.Configuration;
-using Altinn.Notifications.Email.Integrations.Publishers;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using Moq;
 
 using Xunit;
 
