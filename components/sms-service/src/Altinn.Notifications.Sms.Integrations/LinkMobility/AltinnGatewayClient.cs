ebe7e8f86d03dfe4b62e070597a88a1829f921b1
martivj <martin.vagseter.jakobsen@digdir.no>
2026-06-01 12:41:09 +0200

    Catch gateway related errors in call to LM

--- components/sms-service/src/Altinn.Notifications.Sms.Integrations/LinkMobility/AltinnGatewayClient.cs
+++ components/sms-service/src/Altinn.Notifications.Sms.Integrations/LinkMobility/AltinnGatewayClient.cs
@@ -9,21 +9,31 @@ namespace Altinn.Notifications.Sms.Integrations.LinkMobility;
 /// <summary>
 ///  Wrapper class for the LinkMobility SMS Gateway client to support DI
 /// </summary>
-public class AltinnGatewayClient : IAltinnGatewayClient
+/// <remarks>
+/// Initializes a new instance of the <see cref="AltinnGatewayClient"/> class.
+/// </remarks>
+public class AltinnGatewayClient(HttpClient httpClient, SmsGatewaySettings gatewayConfig) : IAltinnGatewayClient
 {
-    private readonly GatewayClient _client;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AltinnGatewayClient"/> class.
-    /// </summary>
-    public AltinnGatewayClient(HttpClient httpClient, SmsGatewaySettings gatewayConfig)
-    {
-        _client = new(new XmlTransport(gatewayConfig.Username, gatewayConfig.Password, new Uri(gatewayConfig.Endpoint), httpClient));
-    }
+    private readonly GatewayClient _client = new(new XmlTransport(gatewayConfig.Username, gatewayConfig.Password, new Uri(gatewayConfig.Endpoint), httpClient));
 
     /// <inheritdoc/>
     public async Task<MessageResult> SendAsync(LinkMobilityModel.Sms message)
     {
-        return await _client.SendAsync(message);
+        try
+        {
+            return await _client.SendAsync(message);
+        }
+        catch (TaskCanceledException ex)
+        {
+            throw new SmsGatewayException("SMS gateway request timed out.", ex);
+        }
+        catch (HttpRequestException ex)
+        {
+            throw new SmsGatewayException("SMS gateway request failed.", ex);
+        }
+        catch (SendMessageException ex)
+        {
+            throw new SmsGatewayException("SMS gateway returned an error response.", ex);
+        }
     }
 }
