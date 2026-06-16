a58862de37bf82ba7fdcd4cb7e2b75be909d59af
Martin Vågseter Jakobsen <martin.vagseter.jakobsen@digdir.no>
2026-06-16 09:32:32 +0200

    Add new foreign key link from orders to orderschain (#1655)
    * Add new orderschain fk column
    
    * Update functions with chainId returns
    
    * Fix localtest auth target
    
    * Fix localtest dummy kvsetting

--- components/api/src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/insertorder.sql
+++ components/api/src/Altinn.Notifications.Persistence/Migration/FunctionsAndProcedures/insertorder.sql
@@ -1,73 +1,4 @@
-CREATE OR REPLACE FUNCTION notifications.insertorder(_alternateid UUID, _creatorname TEXT, _sendersreference TEXT, _created TIMESTAMPTZ, _requestedsendtime TIMESTAMPTZ, _notificationorder JSONB)
-RETURNS BIGINT
-    LANGUAGE 'plpgsql'
-AS $BODY$
-DECLARE
-_orderid BIGINT;
-BEGIN
-	INSERT INTO notifications.orders(alternateid, creatorname, sendersreference, created, requestedsendtime, processed, notificationorder) 
-	VALUES (_alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _created, _notificationorder)
-   RETURNING _id INTO _orderid;
-   
-   RETURN _orderid;
-END;
-$BODY$;
-
--- Postgres supports function overloading - so this function has the same name, but one additional parameter: sendingTimePolicy
-CREATE OR REPLACE FUNCTION notifications.insertorder(
-	_alternateid uuid,
-	_creatorname text,
-	_sendersreference text,
-	_created timestamp with time zone,
-	_requestedsendtime timestamp with time zone,
-	_notificationorder jsonb,
-	_sendingtimepolicy integer)
-    RETURNS bigint
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
-
-DECLARE
-_orderid BIGINT;
-BEGIN
-	INSERT INTO notifications.orders(alternateid, creatorname, sendersreference, created, requestedsendtime, processed, notificationorder, sendingtimepolicy) 
-	VALUES (_alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _created, _notificationorder, _sendingtimepolicy)
-   RETURNING _id INTO _orderid;
-   
-   RETURN _orderid;
-END;
-$BODY$;
-
--- Add new overload that includes the 'type' parameter
-CREATE OR REPLACE FUNCTION notifications.insertorder(
-	_alternateid uuid,
-	_creatorname text,
-	_sendersreference text,
-	_created timestamp with time zone,
-	_requestedsendtime timestamp with time zone,
-	_notificationorder jsonb,
-	_sendingtimepolicy integer,
-	_type text)
-    RETURNS bigint
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
-
-DECLARE
-_orderid BIGINT;
-BEGIN
-	INSERT INTO notifications.orders(alternateid, creatorname, sendersreference, created, requestedsendtime, processed, notificationorder, sendingtimepolicy, type) 
-	VALUES (_alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _created, _notificationorder, _sendingtimepolicy, _type::public.notificationordertype)
-   RETURNING _id INTO _orderid;
-   
-   RETURN _orderid;
-END;
-$BODY$;
-
--- Overload: Adds the 'processedstatus' parameter to allow explicit setting of the order's processing state.
-CREATE OR REPLACE FUNCTION notifications.insertorder(
+CREATE OR REPLACE FUNCTION notifications.insertorder_v2(
     _alternateid uuid,
     _creatorname text,
     _sendersreference text,
@@ -76,7 +7,8 @@ CREATE OR REPLACE FUNCTION notifications.insertorder(
     _notificationorder jsonb,
     _sendingtimepolicy integer,
     _type text,
-    _processingstatus text
+    _processingstatus text,
+    _orderchainid bigint
 )
 RETURNS bigint
 LANGUAGE 'plpgsql'
@@ -85,10 +17,10 @@ DECLARE
     _orderid BIGINT;
 BEGIN
     INSERT INTO notifications.orders(
-        alternateid, creatorname, sendersreference, created, requestedsendtime, processed, notificationorder, sendingtimepolicy, type, processedstatus
+        alternateid, creatorname, sendersreference, created, requestedsendtime, processed, notificationorder, sendingtimepolicy, type, processedstatus, _orderchainid
     )
     VALUES (
-        _alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _created, _notificationorder, _sendingtimepolicy, _type::public.notificationordertype, _processingstatus::public.orderprocessingstate
+        _alternateid, _creatorname, _sendersreference, _created, _requestedsendtime, _created, _notificationorder, _sendingtimepolicy, _type::public.notificationordertype, _processingstatus::public.orderprocessingstate, _orderchainid
     )
     RETURNING _id INTO _orderid;
 
