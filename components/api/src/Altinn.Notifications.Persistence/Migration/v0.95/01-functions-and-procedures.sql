398e44539400ad9f25e989510bef9f7434267114
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-08-10 18:39:44 +0200

    Update regenerated sql script

--- components/api/src/Altinn.Notifications.Persistence/Migration/v0.95/01-functions-and-procedures.sql
+++ components/api/src/Altinn.Notifications.Persistence/Migration/v0.95/01-functions-and-procedures.sql
@@ -2462,7 +2462,44 @@ END;
 $BODY$;
 
 -- insertorderchain.sql:
-CREATE OR REPLACE FUNCTION notifications.insertorderchain_v3(
+CREATE OR REPLACE FUNCTION notifications.insertorderchain_v2(
+    _orderid UUID,
+    _idempotencyid TEXT,
+    _creatorname TEXT,
+    _created TIMESTAMP with time zone,
+    _orderchain JSONB
+)
+RETURNS BIGINT
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    _chainid BIGINT;
+BEGIN
+    INSERT INTO notifications.orderschain(
+        orderid,
+        idempotencyid,
+        creatorname,
+        created,
+        processed,
+        orderchain
+    )
+    VALUES (
+        _orderid,
+        _idempotencyid,
+        _creatorname,
+        _created,
+        _created,
+        _orderchain
+    )
+    RETURNING _id INTO _chainid;
+
+    RETURN _chainid;
+END;
+$BODY$;
+
+
+-- insertorderchainidempotency.sql:
+CREATE OR REPLACE FUNCTION notifications.insertorderchain_idempotency(
     _orderid UUID,
     _idempotencyid TEXT,
     _creatorname TEXT,
@@ -2498,7 +2535,7 @@ BEGIN
 END;
 $BODY$;
 
-COMMENT ON FUNCTION notifications.insertorderchain_v3(UUID, TEXT, TEXT, TIMESTAMP with time zone, JSONB) IS
+COMMENT ON FUNCTION notifications.insertorderchain_idempotency(UUID, TEXT, TEXT, TIMESTAMP with time zone, JSONB) IS
 'Inserts a new order chain row and returns its internal _id, or NULL if a row
 with the same (idempotencyid, creatorname, type) already exists.
 
