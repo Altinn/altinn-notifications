d37691b10d136615a929669f5247940a82f5f658
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-08-11 15:14:40 +0200

    Add drop functions script

---
+++ components/api/src/Altinn.Notifications.Persistence/Migration/v0.95/01-drop-unused-functions.sql
@@ -0,0 +1,27 @@
+DROP FUNCTION IF EXISTS notifications.cancelorder(uuid, text);
+
+DROP FUNCTION IF EXISTS notifications.delete_old_status_feed_records();
+
+DROP FUNCTION IF EXISTS notifications.getemails_statusnew_updatestatus();
+
+DROP FUNCTION IF EXISTS notifications.getmetrics(integer, integer);
+
+DROP FUNCTION IF EXISTS notifications.get_notifications_by_nin(text, timestamp with time zone, timestamp with time zone);
+
+DROP FUNCTION IF EXISTS notifications.getorder_includestatus_v4(uuid, text);
+
+DROP FUNCTION IF EXISTS notifications.getshipmentforstatusfeed(uuid);
+
+DROP FUNCTION IF EXISTS notifications.getshipmentforstatusfeed_v2(uuid);
+
+DROP FUNCTION IF EXISTS notifications.get_shipment_tracking(uuid, text);
+
+DROP FUNCTION IF EXISTS notifications.get_shipment_tracking_v2(uuid, text);
+
+DROP FUNCTION IF EXISTS notifications.getsms_statusnew_updatestatus();
+
+DROP FUNCTION IF EXISTS notifications.getsms_statusnew_updatestatus(integer);
+
+DROP FUNCTION IF EXISTS notifications.updatesmsnotification_v2(text, text, uuid);
+
+DROP PROCEDURE IF EXISTS notifications.updateemailstatus(uuid, text, text);
