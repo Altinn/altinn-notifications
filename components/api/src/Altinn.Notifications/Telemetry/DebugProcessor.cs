54843d268b5ab600297a4357d66345fd4611997e
Normann, Henning <acn-hnorm@ai-dev.no>
2026-08-24 14:11:29 +0200

    Corrected debug processor

--- components/api/src/Altinn.Notifications/Telemetry/DebugProcessor.cs
+++ components/api/src/Altinn.Notifications/Telemetry/DebugProcessor.cs
@@ -10,38 +10,13 @@ using OpenTelemetry;
 namespace Altinn.Notifications.Telemetry
 {
     /// <summary>
-    /// Filter for requests (and child dependencies) that should not be logged.
+    /// Debug filter
     /// </summary>
     /// <remarks>
     /// Initializes a new instance of the <see cref="DebugProcessor"/> class.
     /// </remarks>
     public class DebugProcessor() : BaseProcessor<Activity>()
     {
-        /// <summary>
-        /// Determine whether to skip a request
-        /// </summary>
-        public override void OnStart(Activity activity)
-        {
-            bool skip = false;
-            if (activity.OperationName == RequestKind)
-            {
-                var path = _httpContextAccessor.HttpContext?.Request.Path.Value;
-                if (path != null)
-                {
-                    skip = ExcludeRequest(path);
-                }
-            }
-            else if (!(activity.Parent?.ActivityTraceFlags.HasFlag(ActivityTraceFlags.Recorded) ?? true))
-            {
-                skip = true;
-            }
-
-            if (skip)
-            {
-                activity.ActivityTraceFlags &= ~ActivityTraceFlags.Recorded;
-            }
-        }
-
         /// <summary>
         /// No action on end
         /// </summary>
