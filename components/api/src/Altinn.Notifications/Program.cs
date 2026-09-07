9f7226525877120039b9aafb72e463e55da640af
Holene, Terje <terje.holene@digdir.no>
2026-09-07 13:24:57 +0200

    Add DialogportenClient with dependencies

--- components/api/src/Altinn.Notifications/Program.cs
+++ components/api/src/Altinn.Notifications/Program.cs
@@ -10,6 +10,7 @@ using Altinn.Common.PEP.Authorization;
 using Altinn.Notifications.Authorization;
 using Altinn.Notifications.Configuration;
 using Altinn.Notifications.Core.Extensions;
+using Altinn.Notifications.Core.Shared;
 using Altinn.Notifications.Extensions;
 using Altinn.Notifications.Health;
 using Altinn.Notifications.Integrations.Extensions;
@@ -306,6 +307,7 @@ void AddAuthorizationRulesAndHandlers(IServiceCollection services, IConfiguratio
     services.AddSingleton<IPublicSigningKeyProvider, PublicSigningKeyProvider>();
     services.Configure<Altinn.Common.AccessToken.Configuration.KeyVaultSettings>(config.GetSection("kvSetting"));
     services.AddSingleton<IAuthorizationHandler, AccessTokenHandler>();
+    services.AddSingleton<IAuthenticationContext, AuthenticationContext>();
 }
 
 void AddInputModelValidators(IServiceCollection services)
